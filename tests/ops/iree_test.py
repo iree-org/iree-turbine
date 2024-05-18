cb2a580d35c3460af361e309eb50411abe11976b
Stella Laurenzo <stellaraccident@gmail.com>
2024-05-17 17:55:32 -0700

    Add ops.iree.transfer_to_logical_device() custom op.
    This exposes IREE's multi-device capabilities to PyTorch programs, allowing them to transfer parts of the computation to other devices. This will be used as part of the sharding work in sharktank to do auto placement of sharded tensors (i.e. across GPU devices, etc).
    
    Since this is the first op we have that has different eager vs graph mode semantics, add a new CustomOp.eager_execute() hook which allows the eager execution semantics to be specified separately from graph compilation. Since these local device transfers are only meaningful within an IREE program, they are no-ops during eager execution.
    
    (requires a WIP branch on IREE)
    
    Signed-off-by: Stella Laurenzo <stellaraccident@gmail.com>

--- tests/ops/iree_test.py
+++ tests/ops/iree_test.py
@@ -8,14 +8,48 @@ import logging
 import unittest
 
 import torch
+import torch.nn as nn
 
+import shark_turbine.aot as aot
 import shark_turbine.ops as ops
 
 
-class KernelRegTest(unittest.TestCase):
-    def testTrace(self):
+# See runtime/op_reg/kernel_aot_test.py for additional tests of the trace
+# op.
+class TraceTensorTest(unittest.TestCase):
+    def testEager(self):
         t = torch.randn(3, 4)
         ops.iree.trace_tensor("TEST", t)
+
+    def testAOT(self):
+        class MyModule(nn.Module):
+            def forward(self, x):
+                ops.iree.trace_tensor("TEST", x)
+                return x + 1
+
+        cm = aot.export(MyModule(), args=(torch.empty(9, 8),))
+        asm = str(cm.mlir_module)
+        self.assertIn('flow.tensor.trace "TEST" =', asm)
+
+
+class TransferToLogicalDeviceTest(unittest.TestCase):
+    def testEager(self):
+        t1 = torch.randn(3, 4)
+        t2 = ops.iree.transfer_to_logical_device("1", t1)
+        self.assertIs(t1, t2)
+
+    def testAOT(self):
+        class MyModule(nn.Module):
+            def forward(self, x):
+                y = x + 1
+                z = ops.iree.transfer_to_logical_device("1", y)
+                return z + 1
+
+        cm = aot.export(MyModule(), args=(torch.empty(9, 8),))
+        asm = str(cm.mlir_module)
+        self.assertRegex(
+            asm, "flow.tensor.transfer %.+ to #hal.device.promise<@__device.1>"
+        )
 
 
 if __name__ == "__main__":
