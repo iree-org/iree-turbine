8cda5208ec4b023504474ea1bdf8b3e68090e326
Stella Laurenzo <stellaraccident@gmail.com>
2024-06-13 18:41:52 -0700

    Add workaround for roundtripping 0d tensors in parameter archive.
    Fixes #29
    
    Signed-off-by: Stella Laurenzo <stellaraccident@gmail.com>

--- shark_turbine/aot/params.py
+++ shark_turbine/aot/params.py
@@ -266,9 +266,15 @@ class ParameterArchiveBuilder:
 
     def add_tensor(self, name: str, tensor: torch.Tensor):
         """Adds an named tensor to the archive."""
-        flat_array = tensor.detach().flatten().contiguous().cpu().view(torch.uint8)
-        host_array = flat_array.numpy()
-        self._index.add_buffer(name, host_array, metadata=_make_tensor_metadata(tensor))
+        metadata = _make_tensor_metadata(tensor)
+        # See: https://github.com/iree-org/iree-turbine/issues/29
+        if len(tensor.shape) == 0:
+            flat_array = tensor.detach().cpu().numpy()
+            host_array = bytes(flat_array)
+        else:
+            flat_array = tensor.detach().flatten().contiguous().cpu().view(torch.uint8)
+            host_array = flat_array.numpy()
+        self._index.add_buffer(name, host_array, metadata=metadata)
 
     def add_module(self, module: nn.Module, *, prefix: str = ""):
         """Adds all parameters and persistent buffers from a module hierarchy."""
