9400dfd086ab56bb8dc7bd49154b82759d800bef
Ivan Butygin <ivan.butygin@gmail.com>
2024-08-12 15:03:12 +0200

    move _subs_expr
    Signed-off-by: Ivan Butygin <ivan.butygin@gmail.com>

--- shark_turbine/kernel/lang/wave_types.py
+++ shark_turbine/kernel/lang/wave_types.py
@@ -130,6 +130,24 @@ class Register(metaclass=KernelBufferMeta):
 SymbolsMap: TypeAlias = dict[IndexSymbol, IndexExpr]
 
 
+def _subs_expr(expr: Any, subs: Iterable[tuple[IndexExpr, IndexExpr]]) -> Any:
+    if isinstance(expr, (Symbol, Expr)):
+        return expr.subs(subs)
+
+    return expr
+
+
+def _is_identity_mapping(iters: Iterable[IndexSymbol], mapping: SymbolsMap) -> bool:
+    if len(iters) != len(mapping):
+        return False
+
+    for it, val in zip(iters, mapping.values()):
+        if it != val:
+            return False
+
+    return True
+
+
 class IndexMapping:
     """
     Represents a mapping between 2 sets of indices.
@@ -205,21 +223,3 @@ class IndexMapping:
 
     def is_output_identity(self) -> bool:
         return _is_identity_mapping(self.iters.keys(), self.output_mapping)
-
-
-def _subs_expr(expr: Any, subs: Iterable[tuple[IndexExpr, IndexExpr]]) -> Any:
-    if isinstance(expr, (Symbol, Expr)):
-        return expr.subs(subs)
-
-    return expr
-
-
-def _is_identity_mapping(iters: Iterable[IndexSymbol], mapping: SymbolsMap) -> bool:
-    if len(iters) != len(mapping):
-        return False
-
-    for it, val in zip(iters, mapping.values()):
-        if it != val:
-            return False
-
-    return True
