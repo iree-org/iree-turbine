fb1fcc081652fc5a6e9d38533fa0f4e33f0c0ab8
Ivan Butygin <ivan.butygin@gmail.com>
2024-08-21 15:39:04 +0200

    fix e2e test
    Signed-off-by: Ivan Butygin <ivan.butygin@gmail.com>

--- tests/kernel/wave/wave_e2e_test.py
+++ tests/kernel/wave/wave_e2e_test.py
@@ -17,37 +17,42 @@ def test_copy():
     N = tkl.sym.N
     BLOCK_M = tkl.sym.BLOCK_M
     BLOCK_N = tkl.sym.BLOCK_N
+    ELEMS_PER_THREAD = tkl.sym.ELEMS_PER_THREAD
     ADDRESS_SPACE = tkl.sym.ADDRESS_SPACE
 
     constraints: list[tkw.Constraint] = [
         tkw.HardwareConstraint(
-            threads_per_wave=64, waves_per_block=(1, 1, 1), vector_shapes={M: 16, N: 16}
+            threads_per_wave=64,
+            waves_per_block=(1, 1, 1),
+            vector_shapes={M: 1, N: ELEMS_PER_THREAD},
         )
     ]
     constraints += [tkw.WorkgroupConstraint(M, BLOCK_M, 0)]
     constraints += [tkw.WorkgroupConstraint(N, BLOCK_N, 1)]
-    constraints += [tkw.WaveConstraint(M, BLOCK_M)]
-    constraints += [tkw.WaveConstraint(N, BLOCK_N)]
+    constraints += [tkw.WaveConstraint(M, 64)]
+    constraints += [tkw.WaveConstraint(N, 1)]
 
     @tkw.wave(constraints)
     def test(
         a: tkl.Memory[M, N, ADDRESS_SPACE, tkl.f16],
         b: tkl.Memory[M, N, ADDRESS_SPACE, tkl.f16],
     ):
-        res = tkw.read(a, elements_per_thread=16)
-        tkw.write(res, b, elements_per_thread=16)
+        res = tkw.read(a, elements_per_thread=ELEMS_PER_THREAD)
+        tkw.write(res, b, elements_per_thread=ELEMS_PER_THREAD)
 
     config = {"backend": "rocm", "device": "hip", "target": "gfx942"}
 
-    a = torch.randn(16, 16, dtype=torch.float16)
-    b = torch.zeros(16, 16, dtype=torch.float16)
+    shape = (16, 16)
+    a = torch.randn(shape, dtype=torch.float16)
+    b = torch.zeros(shape, dtype=torch.float16)
     with tk.gen.TestLaunchContext(
         {
-            M: 16,
-            N: 16,
+            M: shape[0],
+            N: shape[1],
             BLOCK_M: 16,
             BLOCK_N: 16,
-            ADDRESS_SPACE: tkl.AddressSpace.SHARED_MEMORY.value,
+            ELEMS_PER_THREAD: 4,
+            ADDRESS_SPACE: tkl.AddressSpace.GLOBAL_MEMORY.value,
         },
         canonicalize=True,
         run=True,
