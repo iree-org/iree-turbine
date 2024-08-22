8a49ec82bef5ef63f12c41c733f3a6696b54af9d
Stanley Winata <stanley.winata@amd.com>
2024-08-22 10:50:58 -0700

    Remove unroll for faster perf
    Signed-off-by: Stanley Winata <stanley.winata@amd.com>

--- tests/kernel/wave/wave_e2e_test.py
+++ tests/kernel/wave/wave_e2e_test.py
@@ -24,8 +24,7 @@ def test_copy():
         tkw.HardwareConstraint(
             threads_per_wave=64,
             waves_per_block=(1, 1, 1),
-            # Select vector shape S.T we unroll read by 4.
-            vector_shapes={M: 1, N: BLOCK_N / 4},
+            vector_shapes={M: 1, N: BLOCK_N},
         )
     ]
     constraints += [tkw.WorkgroupConstraint(M, BLOCK_M, 1)]
