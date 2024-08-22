a33c05b05f216f3e3933a0e3555d660045f3545c
Stanley Winata <stanley.winata@amd.com>
2024-08-22 14:11:02 -0700

    Fix test from CSE integrate
    Signed-off-by: Stanley Winata <stanley.winata@amd.com>

--- shark_turbine/kernel/wave/constraints.py
+++ shark_turbine/kernel/wave/constraints.py
@@ -81,6 +81,8 @@ class HardwareConstraint(Constraint):
 
     # Inline substitution for vector_size given index map. In the future we can add support for other members.
     def _subs(self, index_map: dict[IndexSymbol, int]):
+        if not isinstance(self.vector_shapes, dict):
+            return
         for vector_dim, vector_size in self.vector_shapes.items():
             if isinstance(vector_size, IndexExpr):
                 self.vector_shapes[vector_dim] = vector_size.subs(index_map)
