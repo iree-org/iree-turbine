aa9399c6f168c466a78491fc68c778c0ef23e1f2
Ivan Butygin <ivan.butygin@gmail.com>
2024-08-23 23:17:00 +0200

    fix comment
    Signed-off-by: Ivan Butygin <ivan.butygin@gmail.com>

--- tests/kernel/wave/wave_e2e_test.py
+++ tests/kernel/wave/wave_e2e_test.py
@@ -23,7 +23,7 @@ def test_copy(shape):
     ADDRESS_SPACE = tkl.sym.ADDRESS_SPACE
 
     # Each workgroup works on single row of input data, and rows are further
-    # split into blocks of size up to 256. we always have single wave per WG,
+    # split into blocks of size up to 256. We have single wave per WG,
     # and with default wave size of 64, each thread is operating on up to 4
     # elements.
     wave_size = 64
