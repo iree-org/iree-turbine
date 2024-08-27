91eef4e3536788597e4c95a98d5b6ca3f5362170
Ivan Butygin <ivan.butygin@gmail.com>
2024-08-27 15:31:32 +0200

    im2col test
    Signed-off-by: Ivan Butygin <ivan.butygin@gmail.com>

--- tests/kernel/wave/wave_e2e_test.py
+++ tests/kernel/wave/wave_e2e_test.py
@@ -290,3 +290,103 @@ def test_reduce_max(shape):
     ):
         test(a, b, c)
         assert_allclose(ref.values, c)
+
+
+@require_e2e
+def test_im2col():
+    # TODO: we don't support unaligned access at the moment so all sizes must
+    # be aligned to WG/Wave sizes, c * hw * wf == 8 and number of windows == 64.
+    n, c, h, w = 1, 2, 9, 9  # Image.
+    cf, hf, wf = c, 2, 2  # Filters.
+    padding = 0
+    stride = 1
+
+    sym = tkl.sym
+    ADDRESS_SPACE = sym.ADDRESS_SPACE
+
+    N, C, H, W = sym.N, sym.C, sym.H, sym.W
+    NF, HF, WF = sym.NF, sym.HF, sym.WF
+
+    H_OUT = (H + 2 * padding - HF) // stride + 1
+    W_OUT = (W + 2 * padding - WF) // stride + 1
+    SZ_OUT = H_OUT * W_OUT
+
+    # K = HF * WF * C
+    # M = SZ_OUT * N
+    M = sym.M
+    K = sym.K
+
+    # We unroll K dimension according to ELEMS_PER_THREAD value.
+    # i.e. for K==8 we will have 2 vector.gather's.
+    # Each WG will process 64 windows.
+    wave_size = 64
+    BLOCK_K = hf * wf * c
+    BLOCK_M = 64
+    ELEMS_PER_THREAD = 4
+    i = tkw.IndexMapping.iterator(0)
+    j = tkw.IndexMapping.iterator(1)
+
+    mapping = tkw.IndexMapping(
+        num_iterators=2,
+        inputs={
+            N: i // SZ_OUT,
+            C: j // (HF * WF),
+            H: (i % SZ_OUT) // W_OUT * stride + (j % (HF * WF)) // WF,
+            W: (i % SZ_OUT) % W_OUT * stride + (j % (HF * WF)) % WF,
+        },
+        outputs={M: i, K: j},
+    )
+
+    constraints: list[tkw.Constraint] = [
+        tkw.HardwareConstraint(
+            threads_per_wave=wave_size,
+            waves_per_block=(1, 1, 1),
+            vector_shapes={M: BLOCK_M, K: ELEMS_PER_THREAD},
+        )
+    ]
+    constraints += [tkw.WorkgroupConstraint(M, BLOCK_M, 0)]
+    constraints += [tkw.WorkgroupConstraint(K, BLOCK_K, 1)]
+    constraints += [tkw.WaveConstraint(M, BLOCK_M)]
+    constraints += [tkw.WaveConstraint(K, BLOCK_K)]
+    # TODO: TilingConstraint doesn't work without actual reduction loop, instead
+    # we treat K as WG '1' dimension, but corresponding WG size will be always
+    # equal to 1.
+    # constraints += [tkw.TilingConstraint(K, BLOCK_K)]
+
+    @tkw.wave(constraints)
+    def test(
+        a: tkl.Memory[N, C, H, W, ADDRESS_SPACE, tkl.f16],
+        b: tkl.Memory[M, K, ADDRESS_SPACE, tkl.f16],
+    ):
+        res = tkw.read(a, mapping=mapping, elements_per_thread=ELEMS_PER_THREAD)
+        tkw.write(res, b, elements_per_thread=ELEMS_PER_THREAD)
+
+    config = {"backend": "rocm", "device": "hip", "target": "gfx942"}
+
+    h_out = (h + 2 * padding - hf) // stride + 1
+    w_out = (w + 2 * padding - wf) // stride + 1
+    res_shape = (h_out * w_out * n, hf * wf * c)
+    a = torch.randn((n, c, h, w), dtype=torch.float16)
+    b = torch.zeros(res_shape, dtype=torch.float16)
+
+    im2col = torch.nn.Unfold(kernel_size=(hf, wf), padding=padding, stride=stride)
+    expected = im2col(a)[0, :, :].T
+
+    with tk.gen.TestLaunchContext(
+        {
+            N: n,
+            C: c,
+            W: w,
+            H: h,
+            WF: wf,
+            HF: hf,
+            M: res_shape[0],
+            K: res_shape[1],
+            ADDRESS_SPACE: tkl.AddressSpace.GLOBAL_MEMORY.value,
+        },
+        canonicalize=True,
+        run=True,
+        run_config=config,
+    ):
+        test(a, b)
+        assert_allclose(b, expected)
