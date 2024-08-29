e51be067b094b3ab28c63f5683caf9f7f1457061
Harsh Menon <harsh@nod-labs.com>
2024-08-29 13:19:44 -0700

    Address comments #1
    Signed-off-by: Harsh Menon <harsh@nod-labs.com>

--- shark_turbine/kernel/wave/iree_utils.py
+++ shark_turbine/kernel/wave/iree_utils.py
@@ -1,29 +1,27 @@
 import torch
 from .utils import compile_and_invoke
+from ...support.conversions import TORCH_DTYPE_TO_MLIR_TYPE_ASM
 
 
 def get_mmt_asm(lhs_type: str, rhs_type: str, acc_type: str) -> str:
     acc_dtype = acc_type.split("x")[-1]
-    matmul_function = (
-        f"func.func @mmt(%lhs: tensor<{lhs_type}>, %rhs: tensor<{rhs_type}>) -> tensor<{acc_type}> {{\n"
-        f"  %c0 = arith.constant 0.0 : {acc_dtype}\n"
-        f"  %init = tensor.empty() : tensor<{acc_type}>\n"
-        f"  %inital_result = linalg.fill ins(%c0 : {acc_dtype}) outs(%init : tensor<{acc_type}>) -> tensor<{acc_type}>\n"
-        f"  %result = linalg.matmul_transpose_b ins(%lhs, %rhs: tensor<{lhs_type}>, tensor<{rhs_type}>)\n"
-        f"             outs(%inital_result: tensor<{acc_type}>) -> tensor<{acc_type}>\n"
-        f"  return %result : tensor<{acc_type}>\n"
-        f"}}\n"
-    )
+    matmul_function = f"""
+    func.func @mmt(%lhs: tensor<{lhs_type}>, %rhs: tensor<{rhs_type}>) -> tensor<{acc_type}> {{\n
+      %c0 = arith.constant 0.0 : {acc_dtype}\n
+      %init = tensor.empty() : tensor<{acc_type}>\n
+      %inital_result = linalg.fill ins(%c0 : {acc_dtype}) outs(%init : tensor<{acc_type}>) -> tensor<{acc_type}>\n
+      %result = linalg.matmul_transpose_b ins(%lhs, %rhs: tensor<{lhs_type}>, tensor<{rhs_type}>)\n
+                 outs(%inital_result: tensor<{acc_type}>) -> tensor<{acc_type}>\n
+      return %result : tensor<{acc_type}>\n
+    }}\n"""
     return matmul_function
 
 
 def dtype_str(dtype: torch.dtype) -> str:
-    if dtype == torch.float32:
-        return "f32"
-    elif dtype == torch.float16:
-        return "f16"
-    else:
+    dtype_str = TORCH_DTYPE_TO_MLIR_TYPE_ASM.get(dtype, None)
+    if dtype_str is None:
         raise ValueError(f"Unsupported dtype: {dtype}")
+    return dtype_str
 
 
 def get_type_str(shape: tuple[int], dtype: torch.dtype) -> str:
