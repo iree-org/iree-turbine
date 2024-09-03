6135664af37554853dade30024162b817d7031a9
saienduri <77521230+saienduri@users.noreply.github.com>
2024-09-03 15:49:06 -0700

    Unpin iree (#115)
    We can remove the pinned iree now that the import issue was resolved in
    iree

--- build_tools/post_build_release_test.sh
+++ build_tools/post_build_release_test.sh
@@ -19,6 +19,7 @@ source "${WHEELHOUSE_DIR}"/test.venv/bin/activate
 # --no-index is required so that we don't pick up different versions from pypi
 pip install --no-index -f "${WHEELHOUSE_DIR}" iree-turbine[testing]
 pip install --no-index -f "${WHEELHOUSE_DIR}" torchvision
+pip freeze
 
 # Run tests
 pytest -n 4 "${REPO_ROOT}"
