f07ef64b42fe7b3d79bcdc51ea69eb9bd3b02a24
Stanley Winata <stanley.winata@amd.com>
2024-09-08 09:23:26 -0700

    [TKW] Add register analysis support for ReduceOp
    In order to enable partial reductions/tiling of ReduceOps(max/sum) in a
    for loop/tkw.reduction loop, we need to add register analysis support
    for reduce op.
    
    Signed-off-by: Stanley Winata <stanley.winata@amd.com>

--- shark_turbine/kernel/ops/wave_ops.py
+++ shark_turbine/kernel/ops/wave_ops.py
@@ -382,8 +382,9 @@ class CustomOp(ABC):
 
     def replace_all_uses_with(self, new_node: CustomOp | fx.Node):
         """Replace all uses of the current node with the new node."""
-        for user in self.users:
-            user.update_arg(user.get_node_arg_index(self), new_node)
+        if isinstance(new_node, CustomOp):
+            new_node = new_node.fx_node
+        self.fx_node.replace_all_uses_with(new_node)
 
     def erase(self):
         """Erase the current node from the graph where it exists."""
--- shark_turbine/kernel/wave/register_analysis.py
+++ shark_turbine/kernel/wave/register_analysis.py
@@ -4,15 +4,24 @@
 # See https://llvm.org/LICENSE.txt for license information.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
+from ..wave.constraints import (
+    Constraint,
+    HardwareConstraint,
+    WorkgroupConstraint,
+    TilingConstraint,
+)
+from .._support.indexing import IndexingContext, IndexSequence, IndexSymbol, IndexExpr
 from .._support.tracing import CapturedTrace
 from ...support.logging import get_logger
-from ..ops.wave_ops import get_custom, NewRegister, CustomOp, MMA, Reduction
+from ..ops.wave_ops import get_custom, NewRegister, CustomOp, MMA, Reduction, ReduceOp
 import torch.fx as fx
 
 logger = get_logger("turbine.wave.register_analysis")
 
 
-def set_register_shape(trace: CapturedTrace, custom: CustomOp) -> None:
+def set_register_shape(
+    trace: CapturedTrace, custom: CustomOp, vector_map: dict[IndexSymbol, int]
+) -> None:
     for custom_user in custom.users:
         if isinstance(custom_user, MMA):
             arg_index = custom_user.fx_node.args.index(custom.fx_node)
@@ -39,16 +48,31 @@ def set_register_shape(trace: CapturedTrace, custom: CustomOp) -> None:
                     idx
                 ]
             )
-            set_register_shape(trace, iter_arg)
+            set_register_shape(trace, iter_arg, vector_map)
             custom.fx_node.thread_shape = iter_arg.fx_node.thread_shape
             break
+        elif isinstance(custom_user, ReduceOp):
+            reduced_shape = [
+                dim for dim in custom_user.type.symbolic_shape if dim != custom_user.dim
+            ]
+            register_shape = [
+                vector_map[dim] for dim in reduced_shape if dim in vector_map
+            ]
+            # If rank do not match => some dims not found in hw_constraint.vector_shape.
+            if len(reduced_shape) != len(register_shape):
+                raise NotImplementedError(
+                    "NYI: Handling of dim not in vector_shapes during register analysis."
+                )
+            custom.fx_node.thread_shape = register_shape
         else:
             raise NotImplementedError(
                 f"Register shape propagation not implemented for {custom_user}"
             )
 
 
-def determine_register_shape(trace: CapturedTrace | fx.Graph) -> None:
+def determine_register_shape(
+    trace: CapturedTrace | fx.Graph, constraints: list[Constraint]
+) -> None:
     """
     Each register op is annotated with the wave shape of the register. This
     function determines the thread shape of the register based on the uses
@@ -57,5 +81,10 @@ def determine_register_shape(trace: CapturedTrace | fx.Graph) -> None:
     register_nodes = trace.walk(lambda node: isinstance(get_custom(node), NewRegister))
     if not register_nodes:
         return
+    vector_map = {}
+    for c in constraints:
+        if isinstance(c, HardwareConstraint):
+            vector_map = c.vector_shapes
+            break
     for node in register_nodes:
-        set_register_shape(trace, get_custom(node))
+        set_register_shape(trace, get_custom(node), vector_map)
