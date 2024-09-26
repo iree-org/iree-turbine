6d2fd22320d93c51614e54199b833e7035f28999
Ivan Butygin <ivan.butygin@gmail.com>
2024-09-27 00:31:38 +0300

    [TKW] igemm shared mem tests (#171)
    Signed-off-by: Ivan Butygin <ivan.butygin@gmail.com>
    Signed-off-by: Ian <ian.nordeng@amd.com>

--- tests/kernel/wave/wave_e2e_test.py
+++ tests/kernel/wave/wave_e2e_test.py
@@ -582,7 +582,8 @@ def test_im2col_mma():
 @pytest.mark.parametrize("c", [1, 3, 4, 10])
 @pytest.mark.parametrize("nf", [1, 2, 16])
 @pytest.mark.parametrize("stride", [1, 2, 3])
-def test_igemm_conv(n, c, nf, stride):
+@pytest.mark.parametrize("mem_space", [GLOBAL_ADDRESS_SPACE, SHARED_ADDRESS_SPACE])
+def test_igemm_conv(n, c, nf, stride, mem_space):
     h, w = 5, 5  # Image.
     cf, hf, wf = c, 2, 2  # Filters.
     padding = 0  # TODO: only pad=0 is supported for now
@@ -702,7 +703,7 @@ def test_igemm_conv(n, c, nf, stride):
             BLOCK_M: 16,
             BLOCK_N: 16,
             ELEMS_PER_THREAD: 4,
-            ADDRESS_SPACE: GLOBAL_ADDRESS_SPACE,
+            ADDRESS_SPACE: mem_space,
         },
         canonicalize=True,
         run=True,
