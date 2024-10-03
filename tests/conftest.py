9ed388a6494b2fe27e25436528379013ea16e7c6
Ivan Butygin <ivan.butygin@gmail.com>
2024-10-03 18:28:28 +0300

    [TKW] Fix sympy expr lowering and add some more igemm test shapes (#184)
    * Rework how we are lowering `rational` sympy expressions, instead of
    delayed materialization via lambdas introduce `_Rational` type and
    propagate `numerator/denominator` values independently. Division will
    only be materialized on explicit `sympy.floor/ceiling` op.
    * Rework how igemm test cases are generated and introduce few real
    shapes.
    * Use custom pytest markers to separate perf/non-perf tests
    
    ---------
    
    Signed-off-by: Ivan Butygin <ivan.butygin@gmail.com>

---
+++ tests/conftest.py
@@ -0,0 +1,31 @@
+# Copyright 2024 The IREE Authors
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+import pytest
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--runperf", action="store_true", default=False, help="run performance tests"
+    )
+
+
+def pytest_configure(config):
+    config.addinivalue_line(
+        "markers", "perf_only: performace test, runs only with '--runperf'"
+    )
+
+
+def pytest_collection_modifyitems(config, items):
+    run_perf = config.getoption("--runperf")
+    for item in items:
+        is_perf_only = next(item.iter_markers("perf_only"), None) is not None
+        if run_perf:
+            if not is_perf_only:
+                item.add_marker(pytest.mark.skip("skip non-perf test"))
+        else:
+            if is_perf_only:
+                item.add_marker(pytest.mark.skip("skip perf test"))
