ea9051329a3e5bb38157d073f73cb3bb8d61bc02
Ivan Butygin <ivan.butygin@gmail.com>
2024-10-04 20:09:57 +0300

    [TKW] IGEMM Benchmarking (#187)
    Initial version of IGEMM benchmarking.
    
    * If `--runperf` pytest option is set, generate IREE ref code and run
    both TKW and ref code with `run_bench=True`
    * Add `--dump-perf-files-path` option to save perf info files into
    provided directory (filenames based on test name and params)
    
    ---------
    
    Signed-off-by: Ivan Butygin <ivan.butygin@gmail.com>

--- shark_turbine/kernel/wave/iree_utils.py
+++ shark_turbine/kernel/wave/iree_utils.py
@@ -5,6 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 import torch
+from typing import Any
 from .utils import compile_and_invoke
 from ...support.conversions import TORCH_DTYPE_TO_MLIR_TYPE_ASM
 
@@ -23,6 +24,23 @@ def get_mmt_asm(lhs_type: str, rhs_type: str, acc_type: str) -> str:
     return matmul_function
 
 
+def get_conv_asm(
+    conv_type: str, lhs_type: str, rhs_type: str, res_type: str, stride: int
+) -> str:
+    res_dtype = res_type.split("x")[-1]
+    return f"""
+    func.func @conv_{conv_type}(%lhs: tensor<{lhs_type}>, %rhs: tensor<{rhs_type}>) -> tensor<{res_type}> {{
+      %c0 = arith.constant 0.0 : {res_dtype}
+      %init = tensor.empty() : tensor<{res_type}>
+      %inital_result = linalg.fill ins(%c0 : {res_dtype}) outs(%init : tensor<{res_type}>) -> tensor<{res_type}>
+      %result = linalg.conv_{conv_type}
+                {{dilations = dense<1> : tensor<2xi64>, strides = dense<{stride}> : tensor<2xi64>}}
+                ins(%lhs, %rhs : tensor<{lhs_type}>, tensor<{rhs_type}>)
+                outs(%inital_result : tensor<{res_type}>) -> tensor<{res_type}>
+      return %result : tensor<{res_type}>
+    }}"""
+
+
 def dtype_str(dtype: torch.dtype) -> str:
     dtype_str = TORCH_DTYPE_TO_MLIR_TYPE_ASM.get(dtype, None)
     if dtype_str is None:
@@ -39,20 +57,36 @@ def generate_iree_ref(
     kernel_inputs: list[torch.Tensor],
     kernel_outputs: list[torch.Tensor],
     config: dict[str, str],
+    **kwargs: dict[str, Any],
 ):
     """
     Generate a reference output for the given kernel type and arguments.
     """
 
     asm = None
+    conv_str = "conv_"
     if kernel_type == "mmt":
         lhs_type = get_type_str(kernel_inputs[0].shape, kernel_inputs[0].dtype)
         rhs_type = get_type_str(kernel_inputs[1].shape, kernel_inputs[1].dtype)
         acc_type = get_type_str(kernel_outputs[0].shape, kernel_outputs[0].dtype)
         asm = get_mmt_asm(lhs_type, rhs_type, acc_type)
+    elif kernel_type.startswith(conv_str):
+        lhs_type = get_type_str(kernel_inputs[0].shape, kernel_inputs[0].dtype)
+        rhs_type = get_type_str(kernel_inputs[1].shape, kernel_inputs[1].dtype)
+        acc_type = get_type_str(kernel_outputs[0].shape, kernel_outputs[0].dtype)
+        conv_type = kernel_type[len(conv_str) :]
+        asm = get_conv_asm(
+            conv_type, lhs_type, rhs_type, acc_type, int(kwargs["stride"])
+        )
     else:
         raise ValueError(f"Unknown kernel type: {kernel_type}")
 
     compile_and_invoke(
-        asm, kernel_type, config, kernel_inputs, kernel_outputs, True, False
+        asm,
+        kernel_type,
+        config,
+        kernel_inputs,
+        kernel_outputs,
+        run=True,
+        run_bench=kwargs.get("run_bench", False),
     )
