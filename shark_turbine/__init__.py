876041ccf11791161ce393907561126a887d0224
Ivan Butygin <ivan.butygin@gmail.com>
2024-10-05 17:39:23 +0200

    shark_turbine redirect
    Signed-off-by: Ivan Butygin <ivan.butygin@gmail.com>

---
+++ shark_turbine/__init__.py
@@ -0,0 +1,20 @@
+# Copyright 2024 The IREE Authors
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+
+import warnings
+
+warnings.warn(
+    "'shark_turbine' namespace is deprecated and will be removed soon, pleaase use 'iree.turbine'",
+    DeprecationWarning,
+)
+
+# Temp redirect from old shark_turbine namespace.
+from iree.turbine import aot
+from iree.turbine import dynamo
+from iree.turbine import kernel
+from iree.turbine import ops
+from iree.turbine import runtime
