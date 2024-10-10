6cc6fad9960389c239470b8bd66e5e14c9aa676e
saienduri <77521230+saienduri@users.noreply.github.com>
2024-10-09 23:40:47 -0700

    self and unpacking within a list

--- iree/turbine/kernel/ops/wave_ops.py
+++ iree/turbine/kernel/ops/wave_ops.py
@@ -9,7 +9,6 @@ from typing import (
     Any,
     Callable,
     Optional,
-    Self,
     Sequence,
     Type,
     TypeVar,
@@ -388,7 +387,7 @@ class CustomOp(ABC):
         new_graph: Optional[fx.Graph] = None,
         arg_transform: Optional[Callable[[Any], Any]] = lambda x: x,
         anchor: Optional[fx.Node] = None,
-    ) -> Self:
+    ):
         """Returns a duplicate of this node."""
         graph = new_graph
         if new_graph is None:
@@ -768,7 +767,7 @@ class Allocate(CustomOp):
 
     @property
     def type(self) -> "Memory":
-        return Memory[*self.shape, self.address_space, self.dtype]
+        return None
 
 
 @define_op("shared_memory_barrier")
