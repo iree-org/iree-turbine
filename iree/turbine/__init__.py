df60548d813239136fcf60d6d18e116c24275d77
Marius Brehler <marius.brehler@amd.com>
2024-12-16 21:23:31 +0100

    Remove 'torch' from deps and warn instead
    Instead of enforcing the installation for 'torch' as a dependency, error
    if 'torch' cannot be imported and point the user to how to install.

--- iree/turbine/__init__.py
+++ iree/turbine/__init__.py
@@ -8,3 +8,13 @@ learning models to cloud and edge devices.
 # Licensed under the Apache License v2.0 with LLVM Exceptions.
 # See https://llvm.org/LICENSE.txt for license information.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+import importlib.util
+
+msg = """No module named 'torch'. Follow https://pytorch.org/get-started/locally/#start-locally to install 'torch'.
+For example, on Linux to install with CPU support run:
+  pip3 install torch --index-url https://download.pytorch.org/whl/cpu
+"""
+
+if spec := importlib.util.find_spec("torch") is None:
+    raise ModuleNotFoundError(msg)
