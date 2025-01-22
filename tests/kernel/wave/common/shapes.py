87b3eee5f2d93dbffae991c1a8a3effcec815bfa
Geoffrey Martin-Noble <gcmn@brium.ai>
2025-01-22 11:46:37 -0800

    Give tests parameterized by shapes better names

--- tests/kernel/wave/common/shapes.py
+++ tests/kernel/wave/common/shapes.py
@@ -3,7 +3,7 @@
 # Licensed under the Apache License v2.0 with LLVM Exceptions.
 # See https://llvm.org/LICENSE.txt for license information.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-from .utils import perf_test
+import pytest
 
 # List of all test shapes for end to end tests.
 _e2e_test_shapes = {}
@@ -22,11 +22,13 @@ _e2e_test_shapes["evoformer"] = [
     (1, 512, 256, 8, 8, 256, 8),
 ]
 
-# Add performance test markers.
-for test_name in _e2e_test_shapes:
-    _e2e_test_shapes[test_name] += [perf_test(x) for x in _e2e_test_shapes[test_name]]
-
-
 def get_test_shapes(test_name: str):
     assert test_name in _e2e_test_shapes, f"Unknown test name: {test_name}"
-    return _e2e_test_shapes[test_name]
+    shapes = [
+        pytest.param(s, id="x".join(map(str, s))) for s in _e2e_test_shapes[test_name]
+    ]
+    shapes += [
+        pytest.param(s, id="x".join(map(str, s)) + "-perf", marks=pytest.mark.perf_only)
+        for s in _e2e_test_shapes[test_name]
+    ]
+    return shapes
