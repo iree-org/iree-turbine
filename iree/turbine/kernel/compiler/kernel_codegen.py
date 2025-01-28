e17b0de5de9830e42abe1fbb3228d3f60f33d6e7
Stanley Winata <stanley.winata@amd.com>
2025-01-28 08:36:43 -0800

    remove redundant check
    Signed-off-by: Stanley Winata <stanley.winata@amd.com>

--- iree/turbine/kernel/compiler/kernel_codegen.py
+++ iree/turbine/kernel/compiler/kernel_codegen.py
@@ -220,10 +220,9 @@ class KernelSignature:
         # Extract all placeholder nodes.
         placeholder_nodes = filter_fx_graph(graph, is_placeholder)
 
-        # Sort the arguments based on the original arg_id/ordering during
-        # tracing, if not found during tracing/i.e if it's index symbol
-        # we will just leave it at the end.
-        placeholder_nodes.sort(key=lambda x: x.meta.get("arg_id", 1e6))
+        # Sort the arguments based on the original arg_id/ordering.
+        # This is set when graph was first traced.
+        placeholder_nodes.sort(key=lambda x: x.meta["arg_id"])
         # Create bindings for placeholder nodes.
         for node in placeholder_nodes:
             t = node.type
