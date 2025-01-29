bbc2ce50e8765fa5e467f39c115b078e64de0dd2
Harsh Menon <harsh@nod-labs.com>
2025-01-28 22:56:32 -0800

    Fix lit test after rebase
    Signed-off-by: Harsh Menon <harsh@nod-labs.com>

--- lit_tests/kernel/wave/attention.py
+++ lit_tests/kernel/wave/attention.py
@@ -1171,13 +1171,12 @@ def test_prefill_attention():
         # CHECK-COUNT-1:            vector.store
         # CHECK-COUNT-1:            vector.maskedload
         # CHECK-COUNT-1:            vector.store
-        # CHECK-COUNT-16:           vector.load
+        # CHECK-COUNT-1:            vector.maskedload
+        # CHECK-COUNT-1:            vector.store
+        # CHECK-COUNT-1:            vector.maskedload
+        # CHECK-COUNT-1:            vector.store
+        # CHECK-COUNT-32:           vector.load
         # CHECK-COUNT-16:           amdgpu.mfma
         # CHECK-COUNT-4:            gpu.shuffle xor {{.*}}
-        # CHECK-COUNT-1:            vector.maskedload
-        # CHECK-COUNT-1:            vector.store
-        # CHECK-COUNT-1:            vector.maskedload
-        # CHECK-COUNT-1:            vector.store
-        # CHECK-COUNT-16:           vector.load
         # CHECK-COUNT-16:           amdgpu.mfma
         # CHECK-COUNT-16:      vector.maskedstore
