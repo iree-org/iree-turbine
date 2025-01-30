1e45ec324e60dc26251b3241463311b540389780
Nicolas Vasilache <nicolasvasilache@users.noreply.github.com>
2025-01-30 07:40:44 -0800

    self_index fixes and stress test
    Signed-off-by: Nicolas Vasilache <nicolasvasilache@users.noreply.github.com>

--- iree/turbine/kernel/ops/wave_ops.py
+++ iree/turbine/kernel/ops/wave_ops.py
@@ -44,8 +44,11 @@ def allocate(
 ) -> "Memory":
     ...
 
+
 def self_index(
-    idx: IndexExpr, dtype: DataType
+    idx: IndexExpr,
+    dtype: DataType,
+    elements_per_thread: Optional[IndexExpr | int] = None,
 ) -> "Register":
     ...
 
@@ -727,7 +730,7 @@ class BinaryPyOp(CustomOp, ABC):
         if lhs_dim_set.isdisjoint(rhs_dim_set):
             raise ValueError(
                 "BinaryPyOp requires lhs and rhs shape to be at least broadcastable."
-            )
+                f" got {lhs_type.symbolic_shape} vs {rhs_type.symbolic_shape}")
         broadcasted_type = lhs_type if lhs_dim_set > rhs_dim_set else rhs_type
         self.type = broadcasted_type
 
@@ -944,6 +947,7 @@ class Allocate(CustomOp):
 class SelfIndex(CustomOp):
     idx: IndexExpr
     dtype: DataType
+    elements_per_thread: Optional[IndexExpr | int]
 
     @property
     def indexing_dims(self) -> list[IndexSymbol]:
