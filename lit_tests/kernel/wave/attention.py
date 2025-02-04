e673d3f5238d54f02d403b005f32fde0e7efd401
Harsh Menon <harsh@nod-labs.com>
2025-02-03 16:16:11 -0800

    Remove transpose v in extend attention
    This PR removes the need for the value tensors to be
    permuted for the attention kernel.
    
    Signed-off-by: Harsh Menon <harsh@nod-labs.com>

--- lit_tests/kernel/wave/attention.py
+++ lit_tests/kernel/wave/attention.py
@@ -1259,6 +1259,10 @@ def test_extend_attention():
         # CHECK-COUNT-1:            vector.store
         # CHECK-COUNT-1:            vector.maskedload
         # CHECK-COUNT-1:            vector.store
+        # CHECK-COUNT-1:            vector.gather
+        # CHECK-COUNT-1:            vector.store
+        # CHECK-COUNT-1:            vector.gather
+        # CHECK-COUNT-1:            vector.store
         # CHECK-COUNT-16:           vector.load
         # CHECK-COUNT-16:           amdgpu.mfma
         # CHECK-COUNT-4:            gpu.shuffle xor {{.*}}
@@ -1269,9 +1273,9 @@ def test_extend_attention():
         # CHECK-COUNT-1:            vector.store
         # CHECK-COUNT-1:            vector.maskedload
         # CHECK-COUNT-1:            vector.store
-        # CHECK-COUNT-1:            vector.maskedload
+        # CHECK-COUNT-1:            vector.gather
         # CHECK-COUNT-1:            vector.store
-        # CHECK-COUNT-1:            vector.maskedload
+        # CHECK-COUNT-1:            vector.gather
         # CHECK-COUNT-1:            vector.store
         # CHECK-COUNT-32:           vector.load
         # CHECK-COUNT-16:           amdgpu.mfma
