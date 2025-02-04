061868f3bd1f07ad55268f3ab1951a480921562e
Stanley Winata <stanley.winata@amd.com>
2025-02-03 22:34:03 -0800

    Add GPR_NUM partition support for SelfIndex to enable more MMA intrinsic
    Signed-off-by: Stanley Winata <stanley.winata@amd.com>

--- tests/kernel/wave/attention/vanilla_attention_test.py
+++ tests/kernel/wave/attention/vanilla_attention_test.py
@@ -123,21 +123,12 @@ def testAttention(
 
 @require_e2e
 @pytest.mark.parametrize("shape", get_test_shapes("attention"))
-@pytest.mark.parametrize(
-    "enable_scheduling",
-    [
-        False,
-    ],
-)
-@pytest.mark.parametrize(
-    "dynamic_dims",
-    [
-        False,
-    ],
-)
+@pytest.mark.parametrize("enable_scheduling", [False])
+@pytest.mark.parametrize("dynamic_dims", [False])
 @pytest.mark.parametrize(
     "mfma_variant",
     [
+        (MMAType.F32_32x32x8_F16, MMAType.F32_32x32x8_F16),
         (MMAType.F32_16x16x16_F16, MMAType.F32_16x16x16_F16),
     ],
 )
