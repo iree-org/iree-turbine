b2fc45840e9d770ff586373750160d5a6e690982
Nicolas Vasilache <nicolasvasilache@users.noreply.github.com>
2025-02-12 14:05:11 -0800

    Add RPE variant
    As suggested by @hardcode84, when indirect indexings bottom out into load/store, we can more easily use IndexMapping with complex sympy expressions.
    
    Signed-off-by: Nicolas Vasilache <nicolasvasilache@users.noreply.github.com>

---
+++ playground/attention_with_rpe_test.py
@@ -0,0 +1,218 @@
+# Copyright 2025 The IREE Authors
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+import math
+import torch
+from torch.profiler import profile, record_function, ProfilerActivity
+from torch.nn import functional as F
+from torch.nn.attention.flex_attention import flex_attention
+from torch.nn.attention.flex_attention import create_block_mask
+from torch.testing import assert_close
+from typing import Any, Callable
+
+from iree.turbine.kernel.gen import TestLaunchContext
+from iree.turbine.kernel.wave.constraints import MMAType
+from iree.turbine.kernel.wave.templates.attention_common import AttentionShape
+from iree.turbine.kernel.wave.utils import (
+    device_randn,
+    device_zeros,
+    get_default_run_config,
+    to_default_device,
+)
+from attention_with_rpe_template import (
+    get_vanilla_attention_kernel as get_vanilla_tkw_attention_with_rpe_output_kernel,
+)
+
+torch.manual_seed(0)
+torch.set_printoptions(
+    linewidth=1000000,
+    threshold=1000000,
+    precision=3,
+)
+
+
+### TKW Harness
+def run(fun: Callable, hparams, *args) -> Any:
+    # with torch.profiler.profile(
+    #     activities=[torch.profiler.ProfilerActivity.CUDA]
+    # ) as prof:
+    with torch.no_grad():  # Disable gradient calculations
+        with TestLaunchContext(
+            hparams,
+            canonicalize=True,
+            # compile_config={"print-ir-after": "all"},
+            run=True,
+            run_config=get_default_run_config(),
+            run_bench=False,
+            schedule=False,
+            use_scheduling_barriers=False,
+        ):
+            fun(*args)
+
+# print(
+#     prof.key_averages(group_by_input_shape=True).table(
+#         sort_by="self_cuda_time_total", row_limit=10
+#     )
+# )
+
+
+#################################################################################
+# INIT VALS
+#################################################################################
+# num_query_heads, num_kv_heads, head_size, head_size_kv
+# WARNING: for now 128^n is the minimal size because attention_with_rpe_template
+# does not support padding and as a consequence we have
+shape = AttentionShape(256, 256, 256, 256)
+shape.query_seq_len = 256
+shape.kv_seq_len = 256
+
+assert (
+    shape.num_query_heads == shape.num_kv_heads
+), "expected query and kv to have the same number of heads!"
+
+q_shape = (shape.num_query_heads, shape.query_seq_len, shape.head_size)
+k_shape = (shape.num_kv_heads, shape.kv_seq_len, shape.head_size)
+v_shape = (shape.num_kv_heads, shape.kv_seq_len, shape.head_size_kv)
+o_shape = (shape.num_query_heads, shape.query_seq_len, shape.head_size_kv)
+
+q = device_randn(q_shape, dtype=torch.float16)
+k = device_randn(k_shape, dtype=torch.float16)
+v = device_randn(v_shape, dtype=torch.float16)
+tkw_attention_output = device_zeros(o_shape, dtype=torch.float32)
+tkw_attention_with_rpe_output = device_zeros(o_shape, dtype=torch.float32)
+
+log2e = 1.44269504089
+dk_sqrt = math.sqrt(1.0 / q.shape[-1])
+
+#################################################################################
+# T5 RPE INIT VALS
+#################################################################################
+# T5 RPE parameter
+max_context_length = 10
+
+# Applied pre-softmax on the MMA'ed result so f32.
+# Provision more room for clipping and adding 0 at the boundaries.
+rpe = device_zeros(1000 + max_context_length + 2, dtype=torch.float32)
+rpe = rpe[: max_context_length + 2].view(max_context_length + 2)
+rpe.copy_(device_randn(max_context_length + 2, dtype=torch.float32))
+rpe[0] = 0
+rpe[max_context_length + 1] = 0
+
+tmp_out = device_zeros(shape.num_query_heads, shape.query_seq_len, shape.kv_seq_len, dtype=torch.float32)
+
+def t5_rpe_masked_cond(rpe, max_context_length: int, sequence_length: int, dtype):
+    positions = to_default_device(torch.arange(sequence_length))
+    pos_diff = positions.unsqueeze(1) - positions.unsqueeze(0)
+    mask = to_default_device((pos_diff >= 0) & (pos_diff <= max_context_length))
+    rpe_cond = device_zeros(sequence_length, sequence_length, dtype=dtype)
+    rpe_cond[mask] = rpe[pos_diff[mask]]
+    return rpe_cond
+
+
+# rpe_cond is used by torch only
+rpe_cond = t5_rpe_masked_cond(
+    rpe,
+    max_context_length=max_context_length,
+    sequence_length=shape.kv_seq_len,
+    dtype=tkw_attention_with_rpe_output.dtype,
+)
+
+#################################################################################
+# TKW BASE ATTENTION
+#################################################################################
+### RPE version
+(
+    tkw_attention_with_rpe,
+    hyperparams,
+    dynamic_symbols,
+    dynamic_symbols_map,
+) = get_vanilla_tkw_attention_with_rpe_output_kernel(
+    shape,
+    mfma_variant=[MMAType.F32_16x16x16_F16, MMAType.F32_16x16x16_F16],
+    dynamic_dims=False,
+    max_context_length=max_context_length + 2,
+)
+
+
+def attention_with_rpe(*args):
+    mb = tkw_attention_with_rpe(*args)
+    # print(mb.module_op)
+
+run(
+    attention_with_rpe,
+    hyperparams,
+    q * dk_sqrt * log2e,
+    k,
+    v.permute([0, 2, 1]),
+    rpe * log2e,
+    tkw_attention_with_rpe_output,
+    tmp_out
+)
+
+### Reference version
+(
+    tkw_attention,
+    hyperparams,
+    dynamic_symbols,
+    dynamic_symbols_map,
+) = get_vanilla_tkw_attention_with_rpe_output_kernel(
+    shape,
+    mfma_variant=[MMAType.F32_16x16x16_F16, MMAType.F32_16x16x16_F16],
+    dynamic_dims=False,
+    max_context_length=None, # This will disable RPE
+)
+
+
+def attention(tq, tk, tv, toutput):
+    mb = tkw_attention(tq, tk, tv, toutput)
+    # print(mb.module_op)
+
+
+run(
+    attention,
+    hyperparams,
+    q * dk_sqrt * log2e,
+    k,
+    v.permute([0, 2, 1]),
+    tkw_attention_output,
+)
+
+###
+# Sanity checks, debug masks must match.
+for i in range(tmp_out.shape[0]):
+    assert_close(
+        tmp_out[i, :, :].to(dtype=rpe_cond.dtype),
+        # WARNING: we had to multiply rpe by log2e to account for the FA impl
+        # using exp2. We need to readjust to test the masks.
+        rpe_cond * log2e,
+        atol=2e-3,
+        rtol=2e-3,
+    )
+
+#################################################################################
+# TORCH ATTENTION and ATTENTION + RPE
+#################################################################################
+a = torch.matmul(q, k.transpose(-1, -2)) * dk_sqrt
+torch_attention_output = torch.matmul(torch.softmax(a, dim=-1), v)
+a += rpe_cond.unsqueeze(0)
+torch_attention_with_rpe_output = torch.matmul(F.softmax(a, dim=-1), v)
+
+# Check basic attentions match as we expect.
+assert_close(
+    torch_attention_output.to(dtype=tkw_attention_output.dtype),
+    tkw_attention_output,
+    atol=2e-3,
+    rtol=2e-3,
+)
+
+# Check RPE attentions match as we expect.
+assert_close(
+    torch_attention_with_rpe_output.to(
+        dtype=tkw_attention_with_rpe_output.dtype),
+    tkw_attention_with_rpe_output,
+    atol=2e-3,
+    rtol=2e-3,
+)
