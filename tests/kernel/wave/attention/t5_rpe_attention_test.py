8054323c45a3b16d222c3aac68b48145296b91b0
Ivan Butygin <ivan.butygin@gmail.com>
2025-02-14 17:59:07 +0100

    32x32x8
    Signed-off-by: Ivan Butygin <ivan.butygin@gmail.com>

--- tests/kernel/wave/attention/t5_rpe_attention_test.py
+++ tests/kernel/wave/attention/t5_rpe_attention_test.py
@@ -92,7 +92,10 @@ def create_inputs(
 @pytest.mark.parametrize("dtype", [torch.float16])
 @pytest.mark.parametrize(
     "mfma_variant",
-    [(MMAType.F32_16x16x16_F16, MMAType.F32_16x16x16_F16)],
+    [
+        (MMAType.F32_16x16x16_F16, MMAType.F32_16x16x16_F16),
+        (MMAType.F32_32x32x8_F16, MMAType.F32_32x32x8_F16),
+    ],
 )
 def test_t5_rpe_attention(
     shape: Tuple[int],
