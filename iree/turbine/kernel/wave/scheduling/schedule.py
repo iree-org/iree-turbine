3ae65e61f930cb1a363790a4cd3e2bdf4b0e0c7b
Martin Lücke <martin@brium.ai>
2025-03-13 12:06:26 -0700

    Multi buffering integration - guard multi buffering through an option - remove unused imports - simplify gemm multi buffering test
    Signed-off-by: Martin Lücke <martin@brium.ai>

--- iree/turbine/kernel/wave/scheduling/schedule.py
+++ iree/turbine/kernel/wave/scheduling/schedule.py
@@ -48,6 +48,7 @@ def schedule_reduction(
     constraints: list[Constraint],
     use_scheduling_barriers: bool = False,
     scheduling_type: SchedulingType = SchedulingType.NONE,
+    do_multi_buffering: bool = False,
 ):
     """
     Clones the reduction graph and does the following:
@@ -170,7 +171,8 @@ def schedule_reduction(
     # Update new reduction count.
     new_reduction.count = max_induction_variable - (scheduler.num_stages - 1)
 
-    multi_buffer(trace)
+    if do_multi_buffering:
+        multi_buffer(trace)
 
 
 def schedule_graph(
@@ -178,6 +180,7 @@ def schedule_graph(
     constraints: list[Constraint],
     use_scheduling_barriers: bool = False,
     scheduling_type: SchedulingType = SchedulingType.NONE,
+    do_multi_buffering: bool = False,
 ):
     """
     Given a graph, pipelines the reductions in the graph.
@@ -200,4 +203,5 @@ def schedule_graph(
             constraints,
             use_scheduling_barriers,
             scheduling_type,
+            do_multi_buffering,
         )
