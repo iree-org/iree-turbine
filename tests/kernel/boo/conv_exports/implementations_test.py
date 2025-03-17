5a9d43c7b2e9991e4bdda7641c9294950b9e0f49
zjgarvey <47986913+zjgarvey@users.noreply.github.com>
2025-03-17 17:09:43 -0500

    Implement Conv and backward Conv IR generation (#579)
    First steps in auto-generating some BOO kernels for various convolution
    configurations.
    
    This commit adds some convenience structs for specifying a convolution
    signature, either by parsing an MiOpen driver command string, or by
    instantiation of the `ConvSignature` class in python.
    
    Each `ConvSignature` instance uniquely determines an associated
    `torch.nn.Module`, which is then exported to MLIR.
    
    Next steps:
    
    1. Implement a base `CacheManager` similar to
    <https://github.com/iree-org/iree-turbine/blob/e83bc4f835239fdab5b56f24fda1cc7ac5b93076/iree/turbine/kernel/wave/cache.py#L108>
    from which we can customize hashing.
    2. Define python callable "kernel templates", which should infer a
    signature from pytorch inputs and interact with the cache manager to
    launch a kernel. Think "kernel template" ~ iree-turbine CustomOp + cache
    management.
    3. Tune individual convolution kernels and provide pre-compiled
    high-performance vmfbs. Depending on the difficulty, allow automatically
    compiling and tuning a kernel if not in the cache.
    4. Develop a global template registry for all of our kernel "templates"
    and an API to easily lookup a registered template (e.g., from tkw, boo,
    elsewhere).
    
    ---------
    
    Signed-off-by: zjgarvey <zjgarvey@gmail.com>

---
+++ tests/kernel/boo/conv_exports/implementations_test.py
@@ -0,0 +1,77 @@
+# Copyright 2025 The IREE Authors
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+import pytest
+
+import torch
+from iree.turbine.kernel.boo.conv_exports.conv import Mode, ConvSignature
+
+# Note: Singleton parameters are intentionally included for ease of adding additional configurations to test
+@pytest.mark.parametrize("dtype", [torch.float32])
+@pytest.mark.parametrize("N", [3])
+@pytest.mark.parametrize("C", [2])
+@pytest.mark.parametrize("F", [4])
+@pytest.mark.parametrize("H", [32])
+@pytest.mark.parametrize("W", [16])
+@pytest.mark.parametrize("KH", [2, 3])
+@pytest.mark.parametrize("KW", [1])
+@pytest.mark.parametrize("layout", ["NHWC", "WCHN"])
+@pytest.mark.parametrize("dilation", [1, 2])
+@pytest.mark.parametrize("padding", [0, 2])
+@pytest.mark.parametrize("stride", [1, 3])
+@pytest.mark.parametrize("groups", [1])  # TODO: test with groups!=1 when implemented
+def test_conv_impl(
+    dtype, N, C, F, H, W, KH, KW, layout, dilation, padding, stride, groups
+):
+    to_shape = lambda layout, map: [map[item] for item in layout]
+    in_map = {"N": N, "C": C, "H": H, "W": W}
+    ker_map = {"N": F, "C": C // groups, "H": KH, "W": KW}
+    kwargs = {
+        "input_shape": to_shape(layout, in_map),
+        "kernel_shape": to_shape(layout, ker_map),
+        "dtype": dtype,
+        "input_layout": layout,
+        "kernel_layout": layout,
+        "output_layout": layout,
+        "stride": 2 * [stride],
+        "padding": 2 * [padding],
+        "dilation": 2 * [dilation],
+        "mode": Mode.FORWARD,
+        "groups": groups,
+    }
+    fwd_sig = ConvSignature(**kwargs)
+    x, w = fwd_sig.get_sample_conv_args(seed=1)
+    x = x.to(device="cpu")
+    w = w.to(device="cpu")
+    x.requires_grad_(True)
+    w.requires_grad_(True)
+    fwd = fwd_sig.get_nn_module().to(device="cpu")
+    kwargs["mode"] = Mode.INPUT_BACKWARD
+    bwd_sig = ConvSignature(**kwargs)
+    bwd = bwd_sig.get_nn_module().to(device="cpu")
+    kwargs["mode"] = Mode.WEIGHT_BACKWARD
+    wrw_sig = ConvSignature(**kwargs)
+    wrw = wrw_sig.get_nn_module().to(device="cpu")
+    y = fwd(x, w)
+    y.retain_grad()
+    s = y.sum()
+    s.backward(retain_graph=True)
+    dsdy = y.grad
+    dsdx = bwd(dsdy, w)
+    dsdw = wrw(dsdy, x)
+    rtol = 1e-4
+    atol = 1e-4
+    bwd_match = torch.allclose(dsdx, x.grad, rtol=rtol, atol=atol)
+    wrw_match = torch.allclose(dsdw, w.grad, rtol=rtol, atol=atol)
+    if bwd_match and wrw_match:
+        return
+    if not bwd_match:
+        print(f"{dsdx=}")
+        print(f"{x.grad=}")
+    if not wrw_match:
+        print(f"{dsdw=}")
+        print(f"{w.grad=}")
+    raise RuntimeError(f"{bwd_match=}; {wrw_match=};")
