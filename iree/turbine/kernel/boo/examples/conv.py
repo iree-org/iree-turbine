054a3d81a53a0297071eff6bd2f9aa4efd4c8047
zjgarvey <zjgarvey@gmail.com>
2025-03-18 13:29:35 -0500

    Add a modified conv example to verify hash change works
    Signed-off-by: zjgarvey <zjgarvey@gmail.com>

--- iree/turbine/kernel/boo/examples/conv.py
+++ iree/turbine/kernel/boo/examples/conv.py
@@ -39,9 +39,9 @@ def _run():
         KW = 3
         dtype = torch.float32
         signature = _get_test_signature(N, C, F, H, W, KH, KW)
+        output_types = ((signature.output_shape, dtype),)
         conv = signature.get_nn_module()
         boo_conv = BOOLaunchable("default_conv_2d_fwd", conv)
-        output_types = ((signature.output_shape, dtype),)
 
         # this should print the IR only once (for the first compilation)
         for i in range(10):
@@ -49,6 +49,15 @@ def _run():
             y = boo_conv(*args, output_types=output_types)
             y_ref = conv(*args)
             assert torch.allclose(y, y_ref)
+
+        # this should generate a new hash and re-compile
+        similar_signature = ConvSignature.get(*args, stride=2, shared_layout="NHWC")
+        similar_output_types = ((similar_signature.output_shape, dtype),)
+        similar_conv = similar_signature.get_nn_module()
+        similar_boo_conv = BOOLaunchable("default_conv_2d_fwd", similar_conv)
+        y = similar_boo_conv(*args, output_types=similar_output_types)
+        y_ref = similar_conv(*args)
+        assert torch.allclose(y, y_ref)
 
         # reset the cache with:
         KernelCacheManager.reset("boo", "default_conv_2d_fwd")
