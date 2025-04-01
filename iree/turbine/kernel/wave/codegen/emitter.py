5f8fd9711aa734298fda90b4aa927c91ad371514
Ivan Butygin <ivan.butygin@gmail.com>
2025-04-01 16:14:45 +0200

    switch
    Signed-off-by: Ivan Butygin <ivan.butygin@gmail.com>

--- iree/turbine/kernel/wave/codegen/emitter.py
+++ iree/turbine/kernel/wave/codegen/emitter.py
@@ -197,6 +197,7 @@ def add_emitter_subs(
 
 
 _emulate_ceildiv = False
+_use_affine_expr = False
 
 _Rational = namedtuple("_Rational", ["numerator", "denominator"])
 _ApplyExpr = namedtuple("_ApplyExpr", ["expr", "args"])
@@ -325,12 +326,21 @@ def gen_sympy_index(dynamics: dict[IndexSymbol, Value], expr: sympy.Expr) -> OpR
         return _ApplyExpr(expr, args)
 
     def add_expr(lhs, rhs):
+        if not _use_affine_expr:
+            return addi(lhs, rhs)
+
         return op_expr(lhs, rhs, lambda a, b: a + b)
 
     def muli_expr(lhs, rhs):
+        if not _use_affine_expr:
+            return muli(lhs, rhs)
+
         return op_expr(lhs, rhs, lambda a, b: a * b)
 
     def rem_expr(lhs, rhs):
+        if not _use_affine_expr:
+            return
+
         return op_expr(lhs, rhs, lambda a, b: a % b)
 
     def floordiv_expr(lhs, rhs):
@@ -378,36 +388,41 @@ def gen_sympy_index(dynamics: dict[IndexSymbol, Value], expr: sympy.Expr) -> OpR
             return muli_expr(lhs, rhs)
 
     def _rem(lhs, rhs):
+        if not _use_affine_expr:
+            return arith_d.remsi(*_broadcast(lhs, rhs))
+
         return rem_expr(lhs, rhs)
 
     def _floor(value):
-        if isinstance(value, _Rational):
-            value = floordiv_expr(value.numerator, value.denominator)
+        if not isinstance(value, _Rational):
+            return value
 
-        return value
+        if not _use_affine_expr:
+            return arith_d.divsi(*_broadcast(value.numerator, value.denominator))
+
+        return floordiv_expr(value.numerator, value.denominator)
 
     def _ceiling(value):
-        if isinstance(value, _Rational):
+        if not isinstance(value, _Rational):
+            return value
+
+        if _use_affine_expr:
             return ceildiv_expr(value.numerator, value.denominator)
 
-            if _emulate_ceildiv:
-                # ceildivui(x, y) = x == 0 ? 0 : ((x - 1) / y) + 1
-                one = _get_const(1)
-                zero = _get_const(0)
-                lhs_minus_one = arith_d.subi(*_broadcast(value.numerator, one))
-                div = arith_d.divui(*_broadcast(lhs_minus_one, value.denominator))
-                result = arith_d.addi(*_broadcast(div, one))
-                cmp = arith_d.cmpi(
-                    arith_d.CmpIPredicate.eq, *_broadcast(value.numerator, zero)
-                )
-                zero, result = _broadcast(zero, result)
-                value = arith_d.select(cmp, zero, result)
-            else:
-                value = arith_d.ceildivsi(
-                    *_broadcast(value.numerator, value.denominator)
-                )
-
-        return value
+        if _emulate_ceildiv:
+            # ceildivui(x, y) = x == 0 ? 0 : ((x - 1) / y) + 1
+            one = _get_const(1)
+            zero = _get_const(0)
+            lhs_minus_one = arith_d.subi(*_broadcast(value.numerator, one))
+            div = arith_d.divui(*_broadcast(lhs_minus_one, value.denominator))
+            result = arith_d.addi(*_broadcast(div, one))
+            cmp = arith_d.cmpi(
+                arith_d.CmpIPredicate.eq, *_broadcast(value.numerator, zero)
+            )
+            zero, result = _broadcast(zero, result)
+            return arith_d.select(cmp, zero, result)
+        else:
+            return arith_d.ceildivsi(*_broadcast(value.numerator, value.denominator))
 
     def _group_rationals(stack, count):
         """Group rationals and non-rationals args into 2 contiguous sets.
