3550c75690dafaf8f6c703c6379187e4391f4dde
xintin <gaurav.verma@amd.com>
2025-04-01 16:38:35 +0000

    reverted the changes not part of this pr
    Signed-off-by: xintin <gaurav.verma@amd.com>

--- tests/kernel/wave/attention/vanilla_attention_test.py
+++ tests/kernel/wave/attention/vanilla_attention_test.py
@@ -247,7 +247,9 @@ def testAttentionCausal(
         hyperparams,
         dynamic_symbols,
         dynamic_symbols_map,
-    ) = get_vanilla_attention_kernel(shape, mfma_variant, dynamic_dims, is_causal=True)
+    ) = get_vanilla_attention_kernel(
+        shape, mfma_variant, dynamic_dims, is_causal=True, is_v_transposed=True
+    )
     q_shape = (shape.num_query_heads, shape.query_seq_len, shape.head_size)
     k_shape = (shape.num_kv_heads, shape.kv_seq_len, shape.head_size)
     v_shape = (shape.num_kv_heads, shape.kv_seq_len, shape.head_size_kv)
