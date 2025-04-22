7342f39eb4b89eb8a1dbe7964e5fb29679b19e4c
aviator19941 <avinash.sharma@amd.com>
2025-04-22 16:25:14 -0700

    Rename reduction to iterate after rebase
    Signed-off-by: aviator19941 <avinash.sharma@amd.com>

--- iree/turbine/kernel/wave/templates/vanilla_attention.py
+++ iree/turbine/kernel/wave/templates/vanilla_attention.py
@@ -575,7 +575,7 @@ def get_bhsd_attention_kernel(
 
         # This microkernel encodes the fact that if the reduction
         # dimension were tiled, then we would need to materialize a loop.
-        @tkw.reduction(K2, init_args=[init_max, init_sum, c_reg])
+        @tkw.iterate(K2, init_args=[init_max, init_sum, c_reg])
         def repeat(
             partial_max: tkl.Register[B, H, M, tkl.f32],
             partial_sum: tkl.Register[B, H, M, tkl.f32],
