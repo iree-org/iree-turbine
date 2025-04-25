3747aeb4cce3ac41fa5a40595af1f5cb32ea69d5
xintin <gaurav.verma@amd.com>
2025-04-25 12:07:36 +0000

    clean up
    Signed-off-by: xintin <gaurav.verma@amd.com>

--- iree/turbine/kernel/wave/constraints.py
+++ iree/turbine/kernel/wave/constraints.py
@@ -10,12 +10,9 @@ from enum import Enum
 from typing import Optional, Callable
 from sympy import ceiling, Piecewise, floor, Integer
 
-from .utils.symbol_utils import delinearize_index
-
 from .._support.indexing import IndexExpr, IndexSymbol, IndexSequence
 from .._support.dtype import DataType
 from ..lang.global_symbols import *
-
 
 """
 Formatting for different target intrinsics:
@@ -350,14 +347,6 @@ class HardwareConstraint(Constraint):
             self.threads_per_block[0] * self.threads_per_block[1],
         ]
         return sum([x * y for x, y in zip(thread_ids, threads_per_block)])
-
-    @property
-    def lane_id(self) -> IndexExpr:
-        return self.linearized_thread_id % self.threads_per_wave
-
-    @property
-    def wave_id(self) -> IndexExpr:
-        return delinearize_index(self.linearized_thread_id // 64, self.waves_per_block)
 
     # Inline substitution for vector_size given index map. In the future we can add support for other members.
     def subs_vector_shapes(self, index_map: dict[IndexSymbol, int]):
--- tests/kernel/wave/common/shapes.py
+++ tests/kernel/wave/common/shapes.py
@@ -76,12 +76,6 @@ _e2e_test_shapes["gqa_bshd_decode_attention"] = [
     ),
 ]
 
-_e2e_test_shapes["test_block_reduce"] = [
-    (1, 256),
-    (256, 256),
-    (256, 1024),
-]
-
 test_names = [
     "attention",
     "bhsd_attention",
@@ -93,7 +87,6 @@ test_names = [
     "evoformer",
     "gqa_bshd_attention",
     "gqa_bshd_decode_attention",
-    "test_block_reduce",
 ]
 for test in test_names:
     _perf_test_shapes[test] = _e2e_test_shapes[test]
