d40a4e3d9fd1233500e7d89df8a978e0176190d9
harsh-nod <menonharsh@gmail.com>
2025-04-29 23:52:40 -0700

    [Wave] Add wave_sdpa API for f16 attention (#773)
    This PR adds a wave_sdpa API that is similar to the Pytorch API.
    Internally, it utilizes the vanilla attention template to get the
    kernel.
    
    The template was modified to do the scaling inside the kernel so that
    the query matrix no longer has to be scaled outside the kernel. This was
    modified in all the users of the vanilla template.
    
    Signed-off-by: Harsh Menon <harsh@nod-labs.com>

--- iree/turbine/kernel/wave/nn/functional/__init__.py
+++ iree/turbine/kernel/wave/nn/functional/__init__.py
@@ -4,4 +4,5 @@
 # See https://llvm.org/LICENSE.txt for license information.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
+from .attention import wave_sdpa
 from .quant_attention import wave_sdpa_fp8
