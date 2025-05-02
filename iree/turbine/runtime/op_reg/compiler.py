802c2b00639e29d157bdc21c8ab830c9acf27e00
aviator19941 <avinash.sharma@amd.com>
2025-05-02 09:23:13 -0700

    Initial plumbing for WaveKernelBuilder
    Signed-off-by: aviator19941 <avinash.sharma@amd.com>

--- iree/turbine/runtime/op_reg/compiler.py
+++ iree/turbine/runtime/op_reg/compiler.py
@@ -43,6 +43,7 @@ from ..tracing import tracer
 
 from .base import (
     FreeFuncKernelBuilder,
+    WaveKernelBuilder,
     KernelSelection,
 )
 
@@ -107,6 +108,8 @@ def compile_standalone_kernel(
         async_invocations=async_invocations,
     )
     kb = FreeFuncKernelBuilder.create_module(ksel, func_name=func_name)
+    config = KernelCompileConfig(cache_key, list(device.compile_target_flags))
+    kb = FreeFuncKernelBuilder.create_module(ksel, func_name=func_name)
     with kb.ip, Location.unknown():
         ksel.op.generate(ksel, kb)
 
@@ -123,7 +126,7 @@ def compile_standalone_kernel(
             kb.context,
         )
         func.attributes["preprocessing_pipeline"] = pipeline_attr
-
+    breakpoint()
     kb.module_op.verify()
     # DO NOT SUBMIT: https://github.com/iree-org/iree/issues/17132
     enable_debug_info = False
