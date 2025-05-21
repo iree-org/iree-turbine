a74b8e2a036b9b4d1804a97d24f70d053ba09516
harsh-nod <menonharsh@gmail.com>
2025-05-21 14:22:35 -0700

    [Wave] Add ability to compare against flash_attn_func (#798)
    This PR adds the ability to benchmark the flash_attn_func against wave
    kernels. Also prints additional metadata from profiling.
    
    ---------
    
    Signed-off-by: Harsh Menon <harsh@nod-labs.com>

--- iree/turbine/kernel/wave/perf/benchmark.py
+++ iree/turbine/kernel/wave/perf/benchmark.py
@@ -8,6 +8,7 @@ import argparse
 import json
 import torch
 import iree.turbine.kernel.wave.nn as wave_nn
+from typing import Callable
 
 try:
     from rpdTracerControl import rpdTracerControl
@@ -17,10 +18,17 @@ except ImportError:
     print("rpdTraceControl not found, skipping profiling")
     exit(1)
 
+try:
+    from flash_attn import flash_attn_func
+
+    flash_attn_enabled = True
+except:
+    flash_attn_enabled = False
+
 DEFAULT_OUTPUT_FILENAME = "trace.rpd"
 
 
-def benchmark_wave_sdpa(
+def benchmark_sdpa(
     batch_size: int,
     num_heads: int,
     seq_len_q: int,
@@ -28,6 +36,7 @@ def benchmark_wave_sdpa(
     head_dim: int,
     num_warmup: int,
     num_iterations: int,
+    func: Callable,
     output_filename: str = DEFAULT_OUTPUT_FILENAME,
 ):
     """Benchmark wave_sdpa with given BHSD shapes.
@@ -56,7 +65,7 @@ def benchmark_wave_sdpa(
 
     # Warmup
     for _ in range(num_warmup):
-        _ = wave_nn.functional.wave_sdpa(query, key, value)
+        _ = func(query, key, value)
 
     # Synchronize GPU
     torch.cuda.synchronize()
@@ -68,7 +77,7 @@ def benchmark_wave_sdpa(
 
     # Benchmark with profiling
     for _ in range(num_iterations):
-        _ = wave_nn.functional.wave_sdpa(query, key, value)
+        _ = func(query, key, value)
     torch.cuda.synchronize()
 
     # Stop profiling and get results
@@ -79,6 +88,12 @@ def benchmark_wave_sdpa(
     conn = sqlite3.connect(output_filename)
     df_top = pd.read_sql_query("SELECT * from top", conn)
     df_busy = pd.read_sql_query("SELECT * from busy", conn)
+    df_kernel_info = pd.read_sql_query(
+        "select s1.string, k.stream, k.gridX, k.gridY, k.gridZ, k.workgroupX, k.workgroupY, k.workgroupZ, s2.string from rocpd_kernelapi k left join rocpd_string s1 on k.api_ptr_id = s1.id left join rocpd_string s2 on k.kernelName_id = s2.id;",
+        conn,
+    ).drop_duplicates()
+    print(df_kernel_info)
+    print(df_top)
     conn.close()
     avg_time = df_top["Ave_us"][0] / 1e6
 
@@ -110,8 +125,16 @@ def main():
     parser.add_argument(
         "--num_iterations", type=int, default=100, help="Number of benchmark iterations"
     )
+    parser.add_argument(
+        "--fa", action="store_true", help="Benchmark FA instead of Wave, if available"
+    )
 
     args = parser.parse_args()
+    global flash_attn_enabled
+    if args.fa and not flash_attn_enabled:
+        raise ValueError("FA enabled but not installed on system!")
+
+    flash_attn_enabled = args.fa and flash_attn_enabled
 
     # Load configuration from JSON file
     with open(args.config, "r") as f:
@@ -119,7 +142,7 @@ def main():
 
     # Run benchmark for each configuration
     for shape_config in config["shapes"]:
-        benchmark_wave_sdpa(
+        benchmark_sdpa(
             shape_config["batch_size"],
             shape_config["num_heads"],
             shape_config["seq_len_q"],
@@ -127,6 +150,7 @@ def main():
             shape_config["head_dim"],
             args.num_warmup,
             args.num_iterations,
+            flash_attn_func if flash_attn_enabled else wave_nn.functional.wave_sdpa,
             args.output,
         )
 
