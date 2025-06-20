aced02ce071a98aea1cd6d96e9e44225969872d6
Tim Gymnich <tim@gymni.ch>
2025-06-20 17:23:56 +0000

    improve e2e tests
    Signed-off-by: Tim Gymnich <tim@gymni.ch>

--- tests/kernel/wave/wave_gemm_mxfp_test.py
+++ tests/kernel/wave/wave_gemm_mxfp_test.py
@@ -22,77 +22,8 @@ from .common.utils import require_e2e, require_cdna4
 SCALE_GROUP_SIZE = 32
 
 
-def get_mxfp4_gemm(shape, mfma_variant: ScaledMMAType):
-    # Input sizes
-    M = tkl.sym.M
-    N = tkl.sym.N
-    K = tkl.sym.K
-    # Workgroup tile sizes
-    BLOCK_M = tkl.sym.BLOCK_M
-    BLOCK_N = tkl.sym.BLOCK_N
-    BLOCK_K = tkl.sym.BLOCK_K
-    # Address space (for GPU, shared(1) or global(0))
-    ADDRESS_SPACE = tkl.sym.ADDRESS_SPACE
-
-    # Expose user-constraints
-    constraints: list[tkw.Constraint] = [tkw.WorkgroupConstraint(M, BLOCK_M, 0)]
-    constraints += [tkw.WorkgroupConstraint(N, BLOCK_N, 1)]
-    constraints += [tkw.TilingConstraint(K, BLOCK_K)]
-    constraints += [tkw.WaveConstraint(M, BLOCK_M / 2)]
-    constraints += [tkw.WaveConstraint(N, BLOCK_N / 2)]
-
-    constraints += [
-        tkw.HardwareConstraint(
-            threads_per_wave=64, waves_per_block=(2, 2, 1), mma_type=mfma_variant
-        )
-    ]
-
-    @tkw.wave(constraints)
-    def gemm(
-        a: tkl.Memory[M, K / 2, ADDRESS_SPACE, tkl.i8],
-        a_scale: tkl.Memory[M, K / 32, ADDRESS_SPACE, tkl.i8],
-        b: tkl.Memory[N, K / 2, ADDRESS_SPACE, tkl.i8],
-        b_scale: tkl.Memory[N, K / 32, ADDRESS_SPACE, tkl.i8],
-        c: tkl.Memory[M, N, GLOBAL_ADDRESS_SPACE, tkl.f32],
-    ):
-        c_reg = tkl.Register[M, N, tkl.f32](0.0)
-
-        @tkw.iterate(K, init_args=[c_reg])
-        def repeat(acc: tkl.Register[M, N, tkl.f32]) -> tkl.Register[M, N, tkl.f32]:
-            a_reg = tkw.read(a)
-            a_reg = tkw.bitcast(a_reg, tkl.f4e2m1fn)
-            a_scale_reg = tkw.read(a_scale)
-            a_scale_reg = tkw.bitcast(a_scale_reg, tkl.f8e8m0fnu)
-            b_reg = tkw.read(b)
-            b_reg = tkw.bitcast(b_reg, tkl.f4e2m1fn)
-            b_scale_reg = tkw.read(b_scale)
-            b_scale_reg = tkw.bitcast(b_scale_reg, tkl.f8e8m0fnu)
-            acc = tkw.scaled_mma(a_reg, a_scale_reg, b_reg, b_scale_reg, acc)
-            return acc
-
-        tkw.write(repeat, c)
-
-    hyperparams = {
-        ADDRESS_SPACE: SHARED_ADDRESS_SPACE,
-        BLOCK_M: 32,
-        BLOCK_N: 32,
-        BLOCK_K: 256,
-        M: shape[0],
-        N: shape[1],
-        K: shape[2],
-    }
-    hyperparams.update(get_default_scheduling_params())
-
-    options = WaveCompileOptions(
-        subs=hyperparams,
-        canonicalize=True,
-    )
-    options = set_default_run_config(options)
-    gemm = wave_compile(options, gemm)
-    return gemm
-
-
-def generate_gemm_afp4wfp4_inputs(M, N, K):
+def generate_gemm_afp4wfp4_inputs(shape):
+    M, N, K = shape
     torch.manual_seed(5)
     # 34 is two packed e2m1 values 0010 which is 1.0.
     x_low = torch.randint(0, 16, (M, K // 2), dtype=torch.uint8, device="cuda")
@@ -115,41 +46,24 @@ def generate_gemm_afp4wfp4_inputs(M, N, K):
     return x, w, x_scales, w_scales
 
 
-def get_x_vals():
-    x_vals = [(1024 * v, 1024 * v, 1024 * v) for v in range(1, 9)]
-    x_vals += [(4864, 4096, 8192), (9728, 8192, 65536), (4864, 8192, 4160)]
-    x_vals += [
-        (1, 1280, 8192),
-        (32, 1280, 8192),
-        (64, 1280, 8192),
-        (128, 1280, 8192),
-        (192, 1280, 8192),
-        (256, 1280, 8192),
-        (320, 1280, 8192),
-        (512, 1280, 8192),
-        (1024, 1280, 8192),
-        (2048, 1280, 8192),
-        (4096, 1280, 8192),
-        (8192, 1280, 8192),
-        (16384, 1280, 8192),
-        (1, 8192, 1024),
-        (32, 8192, 1024),
-        (64, 8192, 1024),
-        (128, 8192, 1024),
-        (192, 8192, 1024),
-        (256, 8192, 1024),
-        (320, 8192, 1024),
-        (512, 8192, 1024),
-        (1024, 8192, 1024),
-        (2048, 8192, 1024),
-        (4096, 8192, 1024),
-        (8192, 8192, 1024),
-        (16384, 8192, 1024),
-    ]
-    x_vals += [(2 ** (v - 1), 4096 * v, 4096 * v) for v in range(1, 6)]
-    # x_vals = [(128, 1024, 4096)]
-    x_vals += [(16, 16384, 3328 * 2), (128, 16384, 3328 * 2)]
-    return x_vals
+def generate_gemm_afp8wfp8_inputs(shape):
+    M, N, K = shape
+    torch.manual_seed(5)
+    # 34 is two packed e2m1 values 0010 which is 1.0.
+    x = torch.randn((M, K), dtype=torch.float32, device="cuda").to(torch.float8_e5m2)
+    w = torch.randn((N, K), dtype=torch.float32, device="cuda").to(torch.float8_e5m2)
+    w = w.T
+    # Scale of 1.0 in e8m0, bias 127.
+    x_scales = torch.randint(
+        124, 128, (K // SCALE_GROUP_SIZE, M), dtype=torch.uint8, device="cuda"
+    )
+    w_scales = torch.randint(
+        124, 128, (K // SCALE_GROUP_SIZE, N), dtype=torch.uint8, device="cuda"
+    )
+    x_scales = x_scales.T
+    w_scales = w_scales.T
+
+    return x, w, x_scales, w_scales
 
 
 def mxfp4_to_f32(x):
@@ -185,11 +99,10 @@ def e8m0_to_f32(x):
     return x_f32
 
 
-def run_torch(x, w, x_scales, w_scales, dtype):
+def torchScaledGemmMXFP4(x, w, x_scales, w_scales):
     # First convert the x and w inputs to f32.
     x_f32 = mxfp4_to_f32(x)
     w_f32 = mxfp4_to_f32(w.T)
-    # TODO: Maybe K tile size is broken
     w_f32 = w_f32.T
     # Next convert the e8m0 scales to f32.
     x_scales = x_scales.repeat_interleave(SCALE_GROUP_SIZE, dim=1).to(torch.float32)
@@ -198,31 +111,21 @@ def run_torch(x, w, x_scales, w_scales, dtype):
     w_scales = w_scales.repeat_interleave(SCALE_GROUP_SIZE, dim=1).to(torch.float32)
     w_scales_f32 = e8m0_to_f32(w_scales)
     w_f32 = w_f32 * w_scales_f32.T
-    return torch.mm(x_f32, w_f32).to(dtype)
+    return torch.mm(x_f32, w_f32)
 
 
-@require_e2e
-@require_cdna4
-@pytest.mark.parametrize("M, N, K", [(1024, 1024, 1024), (8192, 8192, 8192)])
-@pytest.mark.parametrize("dtype", [torch.float16, torch.bfloat16])
-@pytest.mark.parametrize(
-    "mfma_variant",
-    [
-        ScaledMMAType.F32_16x16x128_F8F6F4,
-        ScaledMMAType.F32_32x32x64_F8F6F4,
-    ],
-)
-def test_gemm_afp4_wfp4(M: int, N: int, K: int, dtype):
-    x, w, x_scales, w_scales = generate_gemm_afp4wfp4_inputs(M, N, K)
-    out = torch.empty(x.shape[0], w.shape[1], device=x.device, dtype=torch.float32)
-
-    torch_out = run_torch(x, w, x_scales, w_scales, dtype).to(dtype)
-    shape = (M, N, K)
-    gemm = get_mxfp4_gemm(shape)
-    w_t = w.T.contiguous()
-    gemm(x, x_scales, w_t, w_scales, out)
-
-    torch.testing.assert_close(torch_out, out, check_dtype=False)
+def torchScaledGemmMXFP8(x, w, x_scales, w_scales):
+    # First convert the x and w inputs to f32.
+    x_f32 = x.to(torch.float32)
+    w_f32 = w.to(torch.float32)
+    # Next convert the e8m0 scales to f32.
+    x_scales = x_scales.repeat_interleave(SCALE_GROUP_SIZE, dim=1).to(torch.float32)
+    x_scales_f32 = e8m0_to_f32(x_scales)
+    x_f32 = x_f32 * x_scales_f32
+    w_scales = w_scales.repeat_interleave(SCALE_GROUP_SIZE, dim=1).to(torch.float32)
+    w_scales_f32 = e8m0_to_f32(w_scales)
+    w_f32 = w_f32 * w_scales_f32.T
+    return torch.mm(x_f32, w_f32)
 
 
 @require_e2e
@@ -310,7 +213,15 @@ def testScaledGemmMXFP4(
     )
     options = set_default_run_config(options)
     gemm = wave_compile(options, gemm)
-    return gemm
+
+    x, w, x_scales, w_scales = generate_gemm_afp4wfp4_inputs(shape)
+    out = torch.empty(x.shape[0], w.shape[1], device=x.device, dtype=torch.float32)
+
+    torch_out = torchScaledGemmMXFP4(x, w, x_scales, w_scales)
+    w_t = w.T.contiguous()
+    gemm(x, x_scales, w_t, w_scales, out)
+
+    torch.testing.assert_close(torch_out, out, check_dtype=False)
 
 
 @require_e2e
@@ -357,10 +268,10 @@ def testScaledGemmMXFP8(
 
     @tkw.wave(constraints)
     def gemm(
-        a: tkl.Memory[M, K, ADDRESS_SPACE, tkl.f8e5m2],
-        a_scale: tkl.Memory[M, K / 32, ADDRESS_SPACE, tkl.f8e8m0fnu],
-        b: tkl.Memory[N, K, ADDRESS_SPACE, tkl.f8e5m2],
-        b_scale: tkl.Memory[N, K / 32, ADDRESS_SPACE, tkl.f8e8m0fnu],
+        a: tkl.Memory[M, K, ADDRESS_SPACE, tkl.i8],
+        a_scale: tkl.Memory[M, K / 32, ADDRESS_SPACE, tkl.i8],
+        b: tkl.Memory[N, K, ADDRESS_SPACE, tkl.i8],
+        b_scale: tkl.Memory[N, K / 32, ADDRESS_SPACE, tkl.i8],
         c: tkl.Memory[M, N, GLOBAL_ADDRESS_SPACE, tkl.f32],
     ):
         c_reg = tkl.Register[M, N, tkl.f32](0.0)
@@ -368,9 +279,13 @@ def testScaledGemmMXFP8(
         @tkw.iterate(K, init_args=[c_reg])
         def repeat(acc: tkl.Register[M, N, tkl.f32]) -> tkl.Register[M, N, tkl.f32]:
             a_reg = tkw.read(a)
+            a_reg = tkw.bitcast(a_reg, tkl.f8e5m2)
             a_scale_reg = tkw.read(a_scale)
+            a_scale_reg = tkw.bitcast(a_scale_reg, tkl.f8e8m0fnu)
             b_reg = tkw.read(b)
+            b_reg = tkw.bitcast(b_reg, tkl.f8e5m2)
             b_scale_reg = tkw.read(b_scale)
+            b_scale_reg = tkw.bitcast(b_scale_reg, tkl.f8e8m0fnu)
             acc = tkw.scaled_mma(a_reg, a_scale_reg, b_reg, b_scale_reg, acc)
             return acc
 
@@ -394,18 +309,12 @@ def testScaledGemmMXFP8(
     )
     options = set_default_run_config(options)
     gemm = wave_compile(options, gemm)
-    return gemm
 
+    x, w, x_scales, w_scales = generate_gemm_afp8wfp8_inputs(shape)
+    out = torch.empty(x.shape[0], w.shape[1], device=x.device, dtype=torch.float32)
 
-# if __name__ == "__main__":
-#     M, N, K = (1024, 1024, 1024)
-#     dtype = torch.float16
-#     x, w, x_scales, w_scales = generate_gemm_afp4wfp4_inputs(M, N, K)
-#     out = torch.empty(x.shape[0], w.shape[1], device=x.device, dtype=torch.float32)
-#     triton_out = torch.empty(x.shape[0], w.shape[1], device=x.device, dtype=dtype)
+    torch_out = torchScaledGemmMXFP8(x, w, x_scales, w_scales)
+    w_t = w.T.contiguous()
+    gemm(x.to(torch.uint8), x_scales, w_t.to(torch.uint8), w_scales, out)
 
-#     torch_out = run_torch(x, w, x_scales, w_scales, dtype).to(dtype)
-#     shape = (M, N, K)
-#     gemm = get_mxfp4_gemm(shape)
-#     w_t = w.T.contiguous()
-#     gemm(x, x_scales, w_t, w_scales, out)
+    torch.testing.assert_close(torch_out, out, check_dtype=False)
