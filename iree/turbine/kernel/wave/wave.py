807872d0086f0b8c0195e273f4a288bc014274cf
nithinsubbiah <nithinsubbiah@gmail.com>
2025-07-01 20:09:30 +0000

    Reorder gather to shared pass after shared memory index correction
    Signed-off-by: nithinsubbiah <nithinsubbiah@gmail.com>

--- iree/turbine/kernel/wave/wave.py
+++ iree/turbine/kernel/wave/wave.py
@@ -559,8 +559,8 @@ class LaunchableWave(Launchable):
             partial(hoist_loop_invariant_ops, trace, self.constraints),
             partial(global_to_shared_gathers, trace, self.constraints),
             partial(minimize_global_loads, trace, self.constraints),
-            partial(gather_to_shared, trace, self.constraints),
             partial(apply_shared_memory_indexing_corrections, trace, self.constraints),
+            partial(gather_to_shared, trace, self.constraints),
         ]
 
         # Partition strided operators.
