9364b215aa1128dd1a1b02eac20b14eda2d86cd5
Ivan Butygin <ivan.butygin@gmail.com>
2025-07-09 11:59:26 +0200

    func
    Signed-off-by: Ivan Butygin <ivan.butygin@gmail.com>

--- iree/turbine/kernel/wave/compile.py
+++ iree/turbine/kernel/wave/compile.py
@@ -59,7 +59,7 @@ class WaveKernel:
 
             self.launchable = Launchable.from_vm_module(
                 loader,
-                entry_point=options.func_name,
+                entry_point=options.func_name + "$async",
             )
 
     def __call__(self, *args, **kwargs):
