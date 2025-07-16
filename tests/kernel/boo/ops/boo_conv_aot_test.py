b1470db7adda98a85f6316eb4ea51d073a7a9dc5
zjgarvey <zjgarvey@gmail.com>
2025-07-16 08:23:50 -0700

    Remove unittest from tests.
    Signed-off-by: zjgarvey <zjgarvey@gmail.com>

--- tests/kernel/boo/ops/boo_conv_aot_test.py
+++ tests/kernel/boo/ops/boo_conv_aot_test.py
@@ -4,7 +4,7 @@
 # See https://llvm.org/LICENSE.txt for license information.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-import unittest
+import pytest
 import tempfile
 
 from pathlib import Path
@@ -41,56 +41,50 @@ class LayoutCustomizableSample1(torch.nn.Module):
         )
 
 
-class SimpleAOTTest(unittest.TestCase):
-    def testAOTLayoutCustomizable(self):
-        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-        N = 2
-        C = 32
-        H = 16
-        W = 16
-        k = 1
-        f = 2
-        x = torch.randn([N, H, W, C], device=device)
-        w = torch.randn([f, k, k, C], device=device)
-        e = aot.export(LayoutCustomizableSample0(), args=(x, w))
-        e.mlir_module.verify()
-        self.assertIn(
-            "call @conv_2d_float32_forward_2x16x16x32_nhwc_2x1x1x32_fhwc_nhwf_1x1s_0x0p_1x1d_1g",
-            str(e.mlir_module),
-        )
+devices = [torch.device("cpu")] + (
+    [torch.device("cuda:0")] if torch.cuda.is_available() else []
+)
 
-    def testAOTLayoutConvReplacement(self):
-        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-        N = 2
-        C = 32
-        H = 16
-        W = 16
-        k = 1
-        f = 2
-        x = torch.randn([N, C, H, W], device=device).to(
+
+@pytest.mark.parametrize("device", devices)
+def test_AOT_layout_customizable(device: torch.device):
+    N = 2
+    C = 32
+    H = 16
+    W = 16
+    k = 1
+    f = 2
+    x = torch.randn([N, H, W, C], device=device)
+    w = torch.randn([f, k, k, C], device=device)
+    e = aot.export(LayoutCustomizableSample0(), args=(x, w))
+    e.mlir_module.verify()
+    assert (
+        "call @conv_2d_float32_forward_2x16x16x32_nhwc_2x1x1x32_fhwc_nhwf_1x1s_0x0p_1x1d_1g"
+        in str(e.mlir_module)
+    )
+
+
+@pytest.mark.parametrize("device", devices)
+def test_AOT_layout_conv_replacement(device: torch.device):
+    N = 2
+    C = 32
+    H = 16
+    W = 16
+    k = 1
+    f = 2
+    x = torch.randn([N, C, H, W], device=device).to(memory_format=torch.channels_last)
+    w = torch.randn([f, C, k, k], device=device).to(memory_format=torch.channels_last)
+    exported_program = torch.export.export(LayoutCustomizableSample1(), args=(x, w))
+    gm = exported_program.graph_module
+    with tempfile.TemporaryDirectory() as td:
+        LaunchableRuntimeCache.clear()
+        set_cache_dir(Path(td))
+        graph_op = boo_ops.get_custom_graph_op(gm, force_single_dispatch=True)
+        y: torch.Tensor = graph_op(x, w)
+        assert y.is_contiguous(
             memory_format=torch.channels_last
-        )
-        w = torch.randn([f, C, k, k], device=device).to(
-            memory_format=torch.channels_last
-        )
-        exported_program = torch.export.export(LayoutCustomizableSample1(), args=(x, w))
-        gm = exported_program.graph_module
-        with tempfile.TemporaryDirectory() as td:
-            LaunchableRuntimeCache.clear()
-            set_cache_dir(Path(td))
-            graph_op = boo_ops.get_custom_graph_op(gm, force_single_dispatch=True)
-            y: torch.Tensor = graph_op(x, w)
-            self.assertTrue(
-                y.is_contiguous(memory_format=torch.channels_last),
-                msg="Output must be in channels last format.",
-            )
-            cached_items = list(Path.glob(Path(td), "*/"))
-            self.assertEqual(
-                len(cached_items), 1, f"Expected one cached item, got {cached_items}."
-            )
-            p = cached_items[0]
-            self.assertIn("_2x32x16x16xfloat32_cl_2x32x1x1xfloat32_cl", p.name)
-
-
-if __name__ == "__main__":
-    unittest.main()
+        ), "Output must be in channels last format."
+        cached_items = list(Path.glob(Path(td), "*/"))
+        assert len(cached_items) == 1, f"Expected one cached item, got {cached_items}."
+        p = cached_items[0]
+        assert p.name.endswith("_2x32x16x16xfloat32_cl_2x32x1x1xfloat32_cl")
