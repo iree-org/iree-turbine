iree-org/iree-turbine

commit 9b71571671cdfe6f5535cfef3886f7d9b71a39e5

[boo] Always pass output shape to `generic_conv` (#1049)
This avoids having to do a try/catch to handle it not being specified.

iree/turbine/kernel/boo/conv_exports/conv.py
@@ -446,6 +446,7 @@ class ConvForwardCustomGeneric(torch.nn.Module):
         self.wl = str(sig.kernel_layout).lower().replace("n", "f")
         self.ol = str(sig.output_layout).lower().replace("c", "f")
         self.explicit_padding = sig.explicit_padding
+        self.explicit_shape = list(sig.output_shape)
         self.x_pos = sig.input_grouped_dim
         self.w_pos = sig.kernel_grouped_dim
         self.o_pos = sig.output_grouped_dim
@@ -471,7 +472,14 @@ class ConvForwardCustomGeneric(torch.nn.Module):
             w = w.unflatten(self.w_pos, [self.groups, -1])
         x_pad = torch.constant_pad_nd(x, self.explicit_padding, value=0)
         output = generic_conv(
-            x_pad, w, self.stride, self.dilation, self.xl, self.wl, self.ol, []
+            x_pad,
+            w,
+            self.stride,
+            self.dilation,
+            self.xl,
+            self.wl,
+            self.ol,
+            self.explicit_shape,
         ).to(dtype=x_pad.dtype)
         if self.groups != 1:
             output = output.flatten(self.o_pos, self.o_pos + 1)

iree/turbine/ops/conv_fwd.py
@@ -157,7 +157,7 @@ class generic_conv(CustomOp):
         input_layout str,
         weight_layout str,
         output_layout str,
-        output_shape list[]?
+        output_shape list[]
 
     Special Layout Characters:
         the character 'c' is always assumed to correspond to a reduction dimension in the input and weight
@@ -181,7 +181,7 @@ class generic_conv(CustomOp):
         float32 accumulator
     """
 
-    signature = "generic_conv(Tensor x, Tensor w, int[] s, int[] d, str xl, str wl, str ol, int[]? os) -> (Tensor)"
+    signature = "generic_conv(Tensor x, Tensor w, int[] s, int[] d, str xl, str wl, str ol, int[] os) -> (Tensor)"
 
     @no_type_check
     def select(self, ksel: KernelSelection):
@@ -197,26 +197,20 @@ class generic_conv(CustomOp):
         xl_desc = ksel.attr_str(4)
         wl_desc = ksel.attr_str(5)
         ol_desc = ksel.attr_str(6)
+        # output shape
+        os_desc = ksel.attr_list_int(7)
 
         xl = xl_desc.v
         xs = list(x_desc.t.shape)
         wl = wl_desc.v
         ws = list(w_desc.t.shape)
         ol = ol_desc.v
+        os = os_desc.v
 
-        try:
-            os_desc = ksel.attr_list_int(7)
-            os = os_desc.v
-            assert len(os) == len(
-                ol
-            ), f"output size must match output layout. Got {os} for layout {ol}."
-        except Exception as e:
-            logger.debug(
-                "Invalid output shape provided for generic_conv. Falling back to default output shape calculation. Failed with exception %s.",
-                str(e),
-            )
-            os = [-1] * len(ol)
-
+        torch._check(
+            len(os) == len(ol),
+            lambda: f"Output size must match output layout. Got {os} for layout {ol}.",
+        )
         torch._check(
             len(xl) == len(xs),
             lambda: f'Length of input layout should match rank. Got layout "{xl}" for tensor with shape {xs}.',

tests/ops/conv_fwd_test.py
@@ -62,7 +62,7 @@ def testGenericCustomConvImplementationEager(s, d):
     gen.manual_seed(10)
     x = torch.randn([N, H, W, C], generator=gen, dtype=torch.float32, device=device)
     w = torch.randn([F, Hk, Wk, C], generator=gen, dtype=torch.float32, device=device)
-    y = generic_conv(x, w, s, d, "nhwc", "fhwc", "nfhw", [])
+    y = generic_conv(x, w, s, d, "nhwc", "fhwc", "nfhw", [-1] * 4)
     y_expected = torch.convolution(
         x.permute([0, 3, 1, 2]),
         w.permute([0, 3, 1, 2]),

commit 4e5d2fe32c9cf586a870fe1eaa85a1a7a806d782

[boo] Fix ConvForwardCustomGeneric shape logic  with groups (#1063)
This was broken by #1049, this adds a fix + test coverage.

iree/turbine/kernel/boo/conv_exports/conv.py
@@ -458,6 +458,11 @@ class ConvForwardCustomGeneric(torch.nn.Module):
             self.xl = self.xl[: self.x_pos] + "g" + self.xl[self.x_pos :]
             self.wl = self.wl[: self.w_pos] + "g" + self.wl[self.w_pos :]
             self.ol = self.ol[: self.o_pos] + "g" + self.ol[self.o_pos :]
+            self.explicit_shape = (
+                self.explicit_shape[: self.o_pos]
+                + [self.groups, self.explicit_shape[self.o_pos] // self.groups]
+                + self.explicit_shape[self.o_pos + 1 :]
+            )
             pad_g_idx = len(self.explicit_padding) - 1 - 2 * self.x_pos
             self.explicit_padding = (
                 self.explicit_padding[:pad_g_idx]

tests/kernel/boo/conv_exports/implementations_test.py
@@ -79,10 +79,12 @@ def test_conv_impl(
 
 
 @pytest.mark.parametrize("dtype", [torch.float32, torch.float16])
-def test_conv_custom_impl(dtype):
+@pytest.mark.parametrize("groups", [1, 2])
+def test_conv_custom_impl(groups: int, dtype):
     sig = ConvSignature(
         input_shape=[1, 16, 16, 2],
-        kernel_shape=[4, 3, 3, 2],
+        kernel_shape=[4, 3, 3, 2 // groups],
+        groups=groups,
         shared_layout="NHWC",
         dtype=dtype,
     )