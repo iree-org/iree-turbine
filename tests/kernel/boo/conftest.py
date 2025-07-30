de7baf7c6ec608536156a6dce15a6e9e26ebd879
Rahul Kayaith <rkayaith@gmail.com>
2025-07-30 21:35:36 +0000

    license header

--- tests/kernel/boo/conftest.py
+++ tests/kernel/boo/conftest.py
@@ -1,3 +1,8 @@
+# Copyright 2025 The IREE Authors
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 from pathlib import Path
 import pytest
 from iree.turbine.kernel.boo.runtime import set_cache_dir, use_cache_dir
