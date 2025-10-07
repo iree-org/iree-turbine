28e67cec7e2ba0fa5d58312d9f63693faec2e8db
zjgarvey <zjgarvey@gmail.com>
2025-10-07 11:13:28 -0700

    Standardize cache file paths
    Signed-off-by: zjgarvey <zjgarvey@gmail.com>

--- iree/turbine/kernel/boo/runtime/launch.py
+++ iree/turbine/kernel/boo/runtime/launch.py
@@ -21,7 +21,7 @@ from iree.compiler.extras.fx_importer import FxImporter
 from ....support.ir_imports import PassManager, Context
 from ....transforms.general.custom_op_expansion import ExpandCustomOpsPass
 
-from .cache import *
+from .cache import OpCacheFiles, is_cache_enabled, LaunchableRuntimeCache
 from ....aot import export
 from ....importers.ir import Attribute, MLIRError
 from ....runtime import Launchable, Device
@@ -88,8 +88,8 @@ def get_module_asm(
     Returns:
         module_asm: MLIR asm representation of the input torch.nn.Module
     """
-    cache_dir = set_cache_dir() / func_name
-    mlir_path = cache_dir / f"{func_name}.mlir"
+    cache_files = OpCacheFiles(func_name)
+    mlir_path = cache_files.mlir_path
 
     if is_cache_enabled() and mlir_path.is_file():
         logger.debug("Loading cached mlir file at %s", str(mlir_path))
@@ -123,9 +123,7 @@ def get_module_asm(
 
     if is_cache_enabled():
         logger.debug("Saving newly generated mlir file to %s", str(mlir_path))
-        cache_dir = set_cache_dir() / func_name
-        cache_dir.mkdir(parents=True, exist_ok=True)
-        mlir_path = cache_dir / f"{func_name}.mlir"
+        cache_files.base_dir.mkdir(parents=True, exist_ok=True)
         mlir_path.write_text(module_asm)
 
     return module_asm
@@ -138,14 +136,14 @@ def out_of_process_compile(
     func_name: str, key_hashes_and_flags: Sequence[Tuple[str, Sequence[str]]]
 ) -> Tuple[str, Tuple[bool, ...]]:
     """Runs compilation via command line tool. Does not raise an exception if compilation fails."""
-    boo_cache = set_cache_dir()
-    mlir_path = boo_cache / func_name / f"{func_name}.mlir"
+    cache_files = OpCacheFiles(func_name)
+    mlir_path = cache_files.mlir_path
     if not mlir_path.is_file():
         logger.debug("no mlir file found at %s", str(mlir_path))
         return func_name, tuple([False] * len(key_hashes_and_flags))
     success = []
     for key_hash, flags in key_hashes_and_flags:
-        vmfb_path: Path = boo_cache / func_name / f"{key_hash}.vmfb"
+        vmfb_path = cache_files.vmfb_path(key_hash)
         if vmfb_path.is_file():
             logger.debug("found vmfb in cache: %s", str(vmfb_path))
             success.append(True)
@@ -155,7 +153,7 @@ def out_of_process_compile(
             ["iree-compile"] + list(flags) + [str(mlir_path), "-o", str(vmfb_path)]
         )
         command = shlex.join(cl_list)
-        (boo_cache / func_name / f"compile_command_{key_hash}.txt").write_text(command)
+        cache_files.compile_command_log(key_hash).write_text(command)
         logger.debug("compile command:\n%s", command)
         try:
             ret = subprocess.run(command, capture_output=True, shell=True, timeout=10)
@@ -180,16 +178,16 @@ def user_flags_jit_callback(
     """VmModule callback for out-of-process compilation with extra flags provided.
     If boo cache is disabled, this will create temporary files for compilation."""
 
-    boo_cache = set_cache_dir()
+    cache_files = OpCacheFiles(func_name)
 
-    def _compile(flags, mlir_path, vmfb_path):
+    def _compile(flags, mlir_path, vmfb_path, command_log_path):
         cl_list = ["iree-compile"] + flags + [str(mlir_path), "-o", str(vmfb_path)]
         command = shlex.join(cl_list)
-        (vmfb_path.parent / f"compile_command_{vmfb_path.stem}.txt").write_text(command)
+        command_log_path.write_text(command)
         ret = subprocess.run(command, capture_output=True, shell=True, timeout=10)
         if ret.returncode != 0:
             raise RuntimeError(
-                f"Failed compilation for kernel {mlir_path.stem}. "
+                f"Failed compilation for kernel {func_name}. "
                 "If this is a supported BOO op, it's possible compilation failed due to a bad fusion.\n"
                 "Please file an issue at https://github.com/iree-org/iree/issues with the following info.\n"
                 f"SOURCE IR:\n{Path(mlir_path).read_text()}\n."
@@ -200,7 +198,10 @@ def user_flags_jit_callback(
 
     def callback(device):
         key_hash = device.get_type_key_hash()
-        vmfb_path: Path = boo_cache / func_name / f"{key_hash}.vmfb"
+        mlir_path = cache_files.mlir_path
+        vmfb_path = cache_files.vmfb_path(key_hash)
+        command_log_path = cache_files.compile_command_log(key_hash)
+
         vm_instance = device.vm_instance
 
         if is_cache_enabled() and vmfb_path.is_file():
@@ -213,16 +214,15 @@ def user_flags_jit_callback(
         )
 
         if is_cache_enabled():
-            mlir_path = boo_cache / func_name / f"{func_name}.mlir"
             logger.debug("Compiling vmfb to cache: %s", str(vmfb_path))
-            vmfb = _compile(flags, mlir_path, vmfb_path)
+            vmfb = _compile(flags, mlir_path, vmfb_path, command_log_path)
             return VmModule.copy_buffer(vm_instance, vmfb)
 
         with TemporaryDirectory() as td:
             mlir_path = Path(td) / "source.mlir"
             mlir_path.write_text(source)
             vmfb_path = Path(td) / "target.vmfb"
-            vmfb = _compile(flags, mlir_path, vmfb_path)
+            vmfb = _compile(flags, mlir_path, vmfb_path, command_log_path)
             return VmModule.copy_buffer(vm_instance, vmfb)
 
     return callback
@@ -265,13 +265,12 @@ def get_launchable(
     args = arg_factory
     if isinstance(args, Iterable):
         arg_factory = lambda: args
-    cache_dir = set_cache_dir() / func_name if is_cache_enabled() else None
     if cache_only:
         assert (
-            cache_dir is not None
+            is_cache_enabled()
         ), "Cache-only was requested, but the cache is disabled."
         launch = Launchable.from_file_cache_only(
-            cache_dir,
+            OpCacheFiles(func_name).base_dir,
             parameter_providers=(),
             entry_point=f"{func_name}$async",
         )
@@ -297,8 +296,8 @@ def get_module_asm_from_graph_module(
     *,
     force_single_dispatch: bool = False,
 ) -> str:
-    cache_dir = set_cache_dir() / func_name
-    mlir_path = cache_dir / f"{func_name}.mlir"
+    cache_files = OpCacheFiles(func_name)
+    mlir_path = cache_files.mlir_path
 
     if is_cache_enabled() and mlir_path.is_file():
         logger.debug("Loading cached mlir file at %s", str(mlir_path))
@@ -333,9 +332,7 @@ def get_module_asm_from_graph_module(
 
     if is_cache_enabled():
         logger.debug("Saving newly generated mlir file to %s", str(mlir_path))
-        cache_dir = set_cache_dir() / func_name
-        cache_dir.mkdir(parents=True, exist_ok=True)
-        mlir_path = cache_dir / f"{func_name}.mlir"
+        cache_files.base_dir.mkdir(parents=True, exist_ok=True)
         mlir_path.write_text(module_asm)
 
     return module_asm
@@ -349,8 +346,7 @@ def get_launchable_from_graph_module(
     compiler_flags_callback: CompilerFlagsCallback = default_compiler_flags_callback,
 ) -> Launchable:
     """A utility to mimic the behavior of `get_launchable` when we already have a graph module."""
-    session_cache_key = func_name
-    launch = LaunchableRuntimeCache.get(session_cache_key)
+    launch = LaunchableRuntimeCache.get(func_name)
     if launch:
         return launch
     module_asm = get_module_asm_from_graph_module(
@@ -364,5 +360,5 @@ def get_launchable_from_graph_module(
         ),
         entry_point=f"{func_name}$async",
     )
-    LaunchableRuntimeCache.add(session_cache_key, launch)
+    LaunchableRuntimeCache.add(func_name, launch)
     return launch
