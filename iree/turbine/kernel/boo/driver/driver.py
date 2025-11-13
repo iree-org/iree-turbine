d1bc8f6ee25baf249e1d37b281465be86bee982e
Rahul Kayaith <rkayaith@gmail.com>
2025-11-13 16:57:30 -0500

    [boo driver] Improve memory reclamation logic (#1217)
    This contains a few changes:
    - clear the pytorch cache and run garbage collection at the start of
      each benchmark, to reclaim any memory from previous runs
    - make the cleanup threshold device-agnostic by using
      `torch.cuda.get_device_properties`
    - account for memory used by inputs when calculating the number of
      iterations to run before cleanup is necessary
    
    fixes #1216

--- iree/turbine/kernel/boo/driver/driver.py
+++ iree/turbine/kernel/boo/driver/driver.py
@@ -265,18 +265,30 @@ def run(
     """Distributes `iter`-many applications of `func` to `per_device_args`. If
     timing is requested, returns a torch profiler object that can be inspected
     to recover time-related information."""
-    # HIP backend caches allocations by default and can OOM if not explicitly cleared.
-    for device in devices:
-        get_device_from_torch(device).hal_device.allocator.trim()
+
+    def pause_and_collect_mem():
+        for device in devices:
+            torch.cuda.synchronize(device)
+        gc.collect()
+
     # Reset torch.compile caches to avoid hitting re-compile limits.
     torch.compiler.reset()
 
+    # Reclaim all allocations so we have a clean slate. Pytorch and the IREE HIP
+    # backend both cache allocations by default so we need to explicitly clear
+    # them.
+    pause_and_collect_mem()
+    torch.cuda.memory.empty_cache()
+    for device in devices:
+        get_device_from_torch(device).hal_device.allocator.trim()
+
     example_results = func(*per_device_args[0])
     output_num_bytes = sum(x.element_size() * x.numel() for x in example_results)
+    input_num_bytes = sum(x.element_size() * x.numel() for x in per_device_args[0])
     num_devices = len(per_device_args)
-    # This is a rough threshold: Mi300x 192 GB memory divided by 2.
-    mem_bytes_threshold = 96 * (10**9)
-    iter_thresh = int(mem_bytes_threshold // output_num_bytes)
+    # This is a rough threshold: try to only use half the available device memory.
+    mem_bytes_threshold = torch.cuda.get_device_properties(devices[0]).total_memory // 2
+    iter_thresh = (mem_bytes_threshold - input_num_bytes) // output_num_bytes
     assert (
         iter_thresh > 1 or not timing_args.time
     ), "Cannot reliably profile if cleanup is needed after every step."
@@ -334,9 +346,7 @@ def run(
                 print(
                     f">>>\tSynchronizing all devices on iter {iter} and collecting garbage."
                 )
-                for device in devices:
-                    torch.cuda.synchronize(device)
-                gc.collect()
+                pause_and_collect_mem()
             if prof is not None:
                 prof.step()
 
