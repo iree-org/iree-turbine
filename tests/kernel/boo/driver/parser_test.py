bc632651ef56d6cf694569a7f44722f13b97db3c
Keshav Vinayak Jha <keshavvinayakjha@gmail.com>
2026-01-20 04:10:28 +0000

    Added half/float parser_test with sdpa
    Signed-off-by: Keshav Vinayak Jha <keshavvinayakjha@gmail.com>

--- tests/kernel/boo/driver/parser_test.py
+++ tests/kernel/boo/driver/parser_test.py
@@ -32,3 +32,37 @@ def test_aten_parser():
     assert beta_val == 10
     assert alpha_desc.name == "alpha"
     assert alpha_val == 20
+
+
+def test_aten_parser_half():
+    sig = AtenParser.command_to_signature(
+        [
+            "aten::scaled_dot_product_attention",
+            "[[32, 8, 128, 64], [32, 8, 128, 64], [32, 8, 128, 64], [], [], [], [], []]",
+            "['c10::Half', 'c10::Half', 'c10::Half', '', 'Scalar', 'Scalar', '', 'Scalar']",
+            "[[65536, 8192, 64, 1], [65536, 8192, 64, 1], [65536, 8192, 64, 1], [], [], [], [], []]",
+            "['', '', '', 'None', '0.0', 'False', 'None', 'False']",
+        ]
+    )
+
+    [arg_0, arg_1, arg_2] = sig.get_sample_args()
+    assert list(arg_0.shape) == [32, 8, 128, 64]
+    assert list(arg_0.stride()) == [65536, 8192, 64, 1]
+    assert arg_0.dtype == torch.float16
+
+
+def test_aten_parser_float():
+    sig = AtenParser.command_to_signature(
+        [
+            "aten::scaled_dot_product_attention",
+            "[[32, 8, 128, 64], [32, 8, 128, 64], [32, 8, 128, 64], [], [], [], [], []]",
+            "['c10::Float', 'c10::Float', 'c10::Float', '', 'Scalar', 'Scalar', '', 'Scalar']",
+            "[[65536, 8192, 64, 1], [65536, 8192, 64, 1], [65536, 8192, 64, 1], [], [], [], [], []]",
+            "['', '', '', 'None', '0.0', 'False', 'None', 'False']",
+        ]
+    )
+
+    [arg_0, arg_1, arg_2] = sig.get_sample_args()
+    assert list(arg_0.shape) == [32, 8, 128, 64]
+    assert list(arg_0.stride()) == [65536, 8192, 64, 1]
+    assert arg_0.dtype == torch.float32
