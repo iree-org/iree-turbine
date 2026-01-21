d43f8b2eff79b21fcbfdb8a82430d76fc02da5ad
Keshav Vinayak Jha <31160700+keshavvinayak01@users.noreply.github.com>
2026-01-21 14:28:49 +0530

    Added half,float dtype support (#1272)
    `aten.py` was missing support for half/float dtypes, so I added that
    support. Tested with attention locally.
    
    ---------
    
    Signed-off-by: Keshav Vinayak Jha <keshavvinayakjha@gmail.com>
    Co-authored-by: Rahul Kayaith <rkayaith@amd.com>

--- tests/kernel/boo/driver/parser_test.py
+++ tests/kernel/boo/driver/parser_test.py
@@ -1,5 +1,6 @@
 import torch
 from iree.turbine.kernel.boo.op_exports.aten import AtenParser, AtenSignature
+import pytest
 
 
 def test_aten_parser():
@@ -32,3 +33,41 @@ def test_aten_parser():
     assert beta_val == 10
     assert alpha_desc.name == "alpha"
     assert alpha_val == 20
+
+
+@pytest.mark.parametrize(
+    "aten_dtype, expected_dtype",
+    [("c10::Half", torch.float16), ("c10::Float", torch.float32)],
+)
+def test_aten_parser_sdpa(aten_dtype: str, expected_dtype: torch.dtype):
+    sig = AtenParser.command_to_signature(
+        [
+            "aten::scaled_dot_product_attention",
+            "[[32, 8, 128, 64], [32, 8, 128, 64], [32, 8, 128, 64], [], [], [], [], []]",
+            f"['{aten_dtype}', '{aten_dtype}', '{aten_dtype}', '', 'Scalar', 'Scalar', '', 'Scalar']",
+            "[[65536, 8192, 64, 1], [65536, 8192, 64, 1], [65536, 8192, 64, 1], [], [], [], [], []]",
+            "['', '', '', 'None', '0.0', 'False', 'None', 'False']",
+        ]
+    )
+
+    [arg_0, arg_1, arg_2] = sig.get_sample_args()
+    assert list(arg_0.shape) == [32, 8, 128, 64]
+    assert list(arg_0.stride()) == [65536, 8192, 64, 1]
+    assert arg_0.dtype == expected_dtype
+
+
+def test_aten_parser_float():
+    sig = AtenParser.command_to_signature(
+        [
+            "aten::scaled_dot_product_attention",
+            "[[32, 8, 128, 64], [32, 8, 128, 64], [32, 8, 128, 64], [], [], [], [], []]",
+            "['c10::Float', 'c10::Float', 'c10::Float', '', 'Scalar', 'Scalar', '', 'Scalar']",
+            "[[65536, 8192, 64, 1], [65536, 8192, 64, 1], [65536, 8192, 64, 1], [], [], [], [], []]",
+            "['', '', '', 'None', '0.0', 'False', 'None', 'False']",
+        ]
+    )
+
+    [arg_0, arg_1, arg_2] = sig.get_sample_args()
+    assert list(arg_0.shape) == [32, 8, 128, 64]
+    assert list(arg_0.stride()) == [65536, 8192, 64, 1]
+    assert arg_0.dtype == torch.float32
